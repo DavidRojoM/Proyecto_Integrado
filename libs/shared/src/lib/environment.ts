9221f643b24d3b79262d97e3afcbf490869e68e9
David Rojo <davidrojo1997@gmail.com>
2022-05-10 00:16:11 +0200

    build(app): add environment.ts

---
+++ libs/shared/src/lib/environment.ts
@@ -0,0 +1,13 @@
+import { config } from 'dotenv';
+config();
+
+export const ENVIRONMENT = {
+  RMQ_URL: 'amqp://localhost:5672',
+  GATEWAY_PORT: process.env.GATEWAY_PORT || 3000,
+};
+
+export const QUEUES = {
+  GATEWAY: 'GATEWAY_QUEUE',
+  AUTH: 'AUTH_QUEUE',
+  USERS: 'USERS_QUEUE',
+};
