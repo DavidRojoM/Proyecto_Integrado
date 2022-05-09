471c9ad4fdbf1f428afa41ac17cd1806e245ebb2
David Rojo <davidrojo1997@gmail.com>
2022-05-10 00:17:29 +0200

    feat(app): add auth ms

---
+++ apps/ms-auth/src/main.ts
@@ -0,0 +1,29 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ENVIRONMENT, QUEUES } from '@proyecto-integrado/shared';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [ENVIRONMENT.RMQ_URL],
+        queue: QUEUES.AUTH,
+        queueOptions: {
+          durable: true,
+        },
+      },
+    }
+  );
+  app.listen().then(() => {
+    Logger.log(
+      `🚀 Microservice running on: ${ENVIRONMENT.RMQ_URL} AT ${QUEUES.AUTH}`
+    );
+  });
+}
+
+bootstrap();
