4c28ac089bd1224d294f8ba8af560f5467def345
David Rojo <davidrojo1997@gmail.com>
2022-05-10 00:16:28 +0200

    feat(app): add gateway main config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,18 +1,27 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { ENVIRONMENT, QUEUES } from '@proyecto-integrado/shared';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [ENVIRONMENT.RMQ_URL],
+      queue: QUEUES.GATEWAY,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = ENVIRONMENT.GATEWAY_PORT || 3000;
+  await app.startAllMicroservices();
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
