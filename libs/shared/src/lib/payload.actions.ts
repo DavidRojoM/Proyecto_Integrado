b808f87ad15a65d5b54def512f8b154046676e5c
David Rojo <davidrojo1997@gmail.com>
2022-05-18 19:46:24 +0200

    feat(libs/shared): add queue actions

---
+++ libs/shared/src/lib/payload.actions.ts
@@ -0,0 +1,20 @@
+export const PayloadActions = {
+  AUTH: {
+    LOGIN: 'LOGIN',
+    LOGOUT: 'LOGOUT',
+    REFRESH: 'REFRESH',
+    CHECK: 'CHECK',
+  },
+  USERS: {
+    CREATE: 'CREATE',
+    UPDATE: 'UPDATE',
+    DELETE: 'DELETE',
+    FIND_ALL: 'FIND_ALL',
+    FIND_BY_ID: 'FIND_BY_ID',
+    FIND_BY_USERNAME: 'FIND_BY_USERNAME',
+  },
+  MAIL: {
+    SEND: 'SEND',
+    SEND_MANY: 'SEND_MANY',
+  },
+};
