31382447193c05b092c9601462aaac157f2324e0
David Rojo <davidrojo1997@gmail.com>
2022-05-18 19:45:38 +0200

    feat(libs/shared): update environment.ts

--- libs/shared/src/lib/environment.ts
+++ libs/shared/src/lib/environment.ts
@@ -4,6 +4,8 @@ config();
 export const ENVIRONMENT = {
   RMQ_URL: 'amqp://localhost:5672',
   GATEWAY_PORT: process.env.GATEWAY_PORT || 3000,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'secret',
+  JWT_TOKEN_EXPIRATION: process.env.JWT_TOKEN_EXPIRATION || '5m',
 };
 
 export const QUEUES = {
@@ -11,4 +13,5 @@ export const QUEUES = {
   AUTH: 'AUTH_QUEUE',
   USERS: 'USERS_QUEUE',
   IMAGES: 'IMAGES_QUEUE',
+  MAIL: 'MAIL_QUEUE',
 };
