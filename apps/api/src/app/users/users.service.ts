26eca5d0c0d5b853140eafbfed141d50e58d3f01
David Rojo <davidrojo1997@gmail.com>
2022-05-18 23:15:09 +0200

    feat(apps/api): add users service

---
+++ apps/api/src/app/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PayloadActions, UserDto } from '@proyecto-integrado/shared';
+import { firstValueFrom } from 'rxjs';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @Inject('USERS_SERVICE') private readonly usersProxy: ClientProxy
+  ) {}
+
+  findAll() {
+    return firstValueFrom(
+      this.usersProxy.send(PayloadActions.USERS.FIND_ALL, {})
+    );
+  }
+
+  findOne(id: string) {
+    return firstValueFrom(
+      this.usersProxy.send(PayloadActions.USERS.FIND_BY_ID, { id })
+    );
+  }
+
+  signup(user: UserDto): Promise<Partial<UserDto>> {
+    return firstValueFrom(
+      this.usersProxy.send<UserDto, UserDto>(PayloadActions.USERS.CREATE, user)
+    );
+  }
+
+  update(updatedUser: UserDto) {
+    return firstValueFrom(
+      this.usersProxy.send(PayloadActions.USERS.UPDATE, updatedUser)
+    );
+  }
+}
