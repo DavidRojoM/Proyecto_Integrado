69e44522b1b3270afee2192e9c9fe4e4dfa69338
David Rojo <davidrojo1997@gmail.com>
2022-05-18 19:51:47 +0200

    feat(apps/api): change bootstrap message

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   await app.startAllMicroservices();
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `**GATEWAY** Application is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
