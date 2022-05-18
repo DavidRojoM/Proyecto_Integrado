3343f5e2da2de0805ede2b5708d75c240f2318f2
David Rojo <davidrojo1997@gmail.com>
2022-05-18 19:51:17 +0200

    feat(apps/ms-auth): update AppModule

--- apps/ms-auth/src/app/app.module.ts
+++ apps/ms-auth/src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClientsModule } from '@nestjs/microservices';
+import { RMQCONFIG } from '@proyecto-integrado/config';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ENVIRONMENT } from '@proyecto-integrado/shared';
+import { LocalStrategy } from './auth/strategies/local.strategy';
+import { JwtStrategy } from './auth/strategies/jwt.strategy';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ClientsModule.register(RMQCONFIG),
+    JwtModule.register({
+      secret: ENVIRONMENT.JWT_SECRET_KEY,
+      signOptions: {
+        expiresIn: ENVIRONMENT.JWT_TOKEN_EXPIRATION,
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AppModule {}
