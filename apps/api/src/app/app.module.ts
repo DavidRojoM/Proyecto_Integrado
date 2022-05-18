df1041a4b10fac3d7a6e1c86727e9033075660ad
David Rojo <davidrojo1997@gmail.com>
2022-05-18 23:15:35 +0200

    feat(apps/api): update AppModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { RMQCONFIG } from '@proyecto-integrado/config';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { LoggingService } from './shared/services/logging.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [ClientsModule.register(RMQCONFIG)],
-  controllers: [AuthController],
-  providers: [AuthService, LoggingService],
+  controllers: [AuthController, UsersController],
+  providers: [AuthService, LoggingService, UsersService],
 })
 export class AppModule {}
