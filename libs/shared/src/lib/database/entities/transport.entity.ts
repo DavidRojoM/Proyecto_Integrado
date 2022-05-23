7044c15f71eb6077d532d7efa561a824a30b28b1
David Rojo <davidrojo1997@gmail.com>
2022-05-24 00:42:04 +0200

    feat(entities): add Transport entity

---
+++ libs/shared/src/lib/database/entities/transport.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Transports } from '@proyecto-integrado/shared';
+import { TripEntity } from './trip.entity';
+
+@Entity('transports')
+export class TransportEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: Transports,
+  })
+  type: Transports;
+
+  @Column({
+    type: 'decimal',
+  })
+  price: number;
+
+  @Column()
+  brand: string;
+
+  @OneToMany((type) => TripEntity, (trip) => trip.destination)
+  trip: TripEntity;
+}
