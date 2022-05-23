f1e66ddc5550b6c185d16e5fedd6c42b2dfb2216
David Rojo <davidrojo1997@gmail.com>
2022-05-24 00:44:53 +0200

    feat(libs/shared): add User Dto

--- libs/shared/src/lib/domain/dto/users/user.dto.ts
+++ libs/shared/src/lib/domain/dto/users/user.dto.ts
@@ -1,8 +1,17 @@
-//TODO: Make it a class
-export interface UserDto {
+import { PartyDto } from '../parties/party.dto';
+import { MessageDto } from '../comms/message.dto';
+import { Roles } from '../../enums/roles.enum';
+
+export class UserDto {
   id: string;
   email: string;
   username: string;
-  roles: any;
+  role: Roles;
   password: string;
+  banned: boolean;
+  image?: string;
+  bankAccount: string;
+  nationality: string;
+  parties: PartyDto[];
+  messages: MessageDto[];
 }
