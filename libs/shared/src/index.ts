bc99e882c3f10b9fb9767c8b7ee6623a7559d6d2
David Rojo <davidrojo1997@gmail.com>
2022-05-24 00:46:04 +0200

    feat(libs/shared): export domain to barrel file

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,9 +1,37 @@
 export * from './lib/shared.module';
 export * from './lib/environment';
+
 export * from './lib/payload.actions';
 export * from './lib/payloads/auth/auth.payloads.interface';
 export * from './lib/payloads/users/users.payloads.interface';
-export * from './lib/domain/interfaces/users/user.interface';
-export * from './lib/domain/dto/users/user.dto';
+
 export * from './lib/domain/enums/roles.enum';
 export * from './lib/domain/enums/transports.enum';
+
+export * from './lib/domain/models/users/user.model';
+export * from './lib/domain/models/parties/party.model';
+export * from './lib/domain/models/comms/message.model';
+export * from './lib/domain/models/trips/trip.model';
+export * from './lib/domain/models/trips/destination.model';
+export * from './lib/domain/models/trips/hotel.model';
+export * from './lib/domain/models/trips/transport.model';
+
+export * from './lib/domain/dto/trips/trip.dto';
+export * from './lib/domain/dto/parties/party.dto';
+export * from './lib/domain/dto/comms/message.dto';
+export * from './lib/domain/dto/trips/hotel.dto';
+export * from './lib/domain/dto/trips/destination.dto';
+export * from './lib/domain/dto/trips/transport.dto';
+export * from './lib/domain/dto/users/user.dto';
+
+export * from './lib/database/entities.module';
+export * from './lib/database/entities/user.entity';
+export * from './lib/database/entities/transport.entity';
+export * from './lib/database/entities/community.entity';
+export * from './lib/database/entities/hotel.entity';
+export * from './lib/database/entities/party.entity';
+export * from './lib/database/entities/message.entity';
+export * from './lib/database/entities/destination.entity';
+export * from './lib/database/entities/trip.entity';
+
+export * from './lib/database/entities';
