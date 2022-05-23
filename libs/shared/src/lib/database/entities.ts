ce7141a7de982a09141257dbb5c8f867e4ec24b7
David Rojo <davidrojo1997@gmail.com>
2022-05-24 00:43:24 +0200

    feat(entities): add entities array

---
+++ libs/shared/src/lib/database/entities.ts
@@ -0,0 +1,19 @@
+import { CommunityEntity } from './entities/community.entity';
+import { DestinationEntity } from './entities/destination.entity';
+import { HotelEntity } from './entities/hotel.entity';
+import { MessageEntity } from './entities/message.entity';
+import { PartyEntity } from './entities/party.entity';
+import { TransportEntity } from './entities/transport.entity';
+import { TripEntity } from './entities/trip.entity';
+import { UserEntity } from './entities/user.entity';
+
+export const entities = [
+  CommunityEntity,
+  DestinationEntity,
+  HotelEntity,
+  MessageEntity,
+  PartyEntity,
+  TransportEntity,
+  TripEntity,
+  UserEntity,
+];
