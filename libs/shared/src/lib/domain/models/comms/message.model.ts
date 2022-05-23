db297d6b7c295d48feaf0040738b432fc31c6a2a
David Rojo <davidrojo1997@gmail.com>
2022-05-24 00:45:15 +0200

    feat(libs/shared): add Message model

---
+++ libs/shared/src/lib/domain/models/comms/message.model.ts
@@ -0,0 +1,40 @@
+import { User } from '../users/user.model';
+import { Party } from '../parties/party.model';
+import { MessageDto } from '../../dto/comms/message.dto';
+import { MessageEntity } from '../../../database/entities/message.entity';
+
+export class Message {
+  message: string;
+  createdAt: Date;
+  user: User;
+  party: Party;
+
+  static entityToModel(message: MessageEntity): Message {
+    const model = new Message();
+    model.message = message.message;
+    model.createdAt = message.createdAt;
+    model.user = User.entityToModel(message.user);
+    model.party = Party.entityToModel(message.party);
+
+    return model;
+  }
+
+  static modelToEntity(message: Message): MessageEntity {
+    const entity = new MessageEntity();
+    entity.message = message.message;
+    entity.createdAt = message.createdAt;
+    entity.user = User.modelToEntity(message.user);
+    entity.party = Party.modelToEntity(message.party);
+
+    return entity;
+  }
+
+  static modelToDto(message: Message): MessageDto {
+    const dto = new MessageDto();
+    dto.message = message.message;
+    dto.createdAt = message.createdAt;
+    dto.user = User.modelToDto(message.user);
+    dto.party = Party.modelToDto(message.party);
+    return dto;
+  }
+}
