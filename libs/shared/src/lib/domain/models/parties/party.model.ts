18e11d8af716e91ccfb904db7d0e76112a497782
David Rojo <davidrojo1997@gmail.com>
2022-05-24 00:45:21 +0200

    feat(libs/shared): add Party model

---
+++ libs/shared/src/lib/domain/models/parties/party.model.ts
@@ -0,0 +1,46 @@
+import { Message } from '../comms/message.model';
+import { User } from '../users/user.model';
+import { Trip } from '../trips/trip.model';
+import { PartyDto } from '../../dto/parties/party.dto';
+import { PartyEntity } from '../../../database/entities/party.entity';
+
+export class Party {
+  id: string;
+  name: string;
+  messages: Message[];
+  users: User[];
+  trip: Trip;
+
+  static modelToEntity(model: Party): PartyEntity {
+    const entity = new PartyEntity();
+    entity.id = model.id;
+    entity.name = model.name;
+    entity.messages = model.messages.map((message) =>
+      Message.modelToEntity(message)
+    );
+    entity.trip = Trip.modelToEntity(model.trip);
+    entity.users = model.users.map((user) => User.modelToEntity(user));
+    return entity;
+  }
+
+  static entityToModel(entity: PartyEntity): Party {
+    const model = new Party();
+    model.id = entity.id;
+    model.name = entity.name;
+    model.messages = entity.messages.map((message) =>
+      Message.entityToModel(message)
+    );
+    model.trip = Trip.entityToModel(entity.trip);
+    model.users = entity.users.map((user) => User.entityToModel(user));
+    return model;
+  }
+
+  static modelToDto(party: Party): PartyDto {
+    const dto = new PartyDto();
+    dto.id = party.id;
+    dto.name = party.name;
+    dto.messages = party.messages.map((message) => Message.modelToDto(message));
+    dto.trip = Trip.modelToDto(party.trip);
+    return dto;
+  }
+}
