555135fca27fd427a5079579d4280995589f4aa7
David Rojo <davidrojo1997@gmail.com>
2022-05-24 23:42:35 +0200

    feat(apps/ms-images): add port listening

--- apps/ms-images/src/main.ts
+++ apps/ms-images/src/main.ts
@@ -1,28 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import { ENVIRONMENT, QUEUES } from '@proyecto-integrado/shared';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [ENVIRONMENT.RMQ_URL],
-        queue: QUEUES.IMAGES,
-        queueOptions: {
-          durable: true,
-        },
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [ENVIRONMENT.RMQ_URL],
+      queue: QUEUES.IMAGES,
+      queueOptions: {
+        durable: true,
       },
-    }
-  );
-  app.listen().then(() => {
-    Logger.log(
-      `**IMAGES** Microservice running on: ${ENVIRONMENT.RMQ_URL} AT ${QUEUES.IMAGES}`
-    );
+    },
   });
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = ENVIRONMENT.IMAGES_PORT || 3001;
+  await app.startAllMicroservices();
+  await app.listen(port);
+  Logger.log(
+    `**IMAGES** Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
