f5fea5bd11d4d2b11e14613346ae0fffed7cb464
David Rojo <davidrojo1997@gmail.com>
2022-05-26 23:26:41 +0200

    feat(apps/api): add logginService impl

--- apps/api/src/app/shared/services/logging.service.ts
+++ apps/api/src/app/shared/services/logging.service.ts
@@ -1,13 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { promisify } from 'util';
-import { writeFile } from 'fs/promises';
-
-const promiseWriteFIle = promisify(writeFile);
+import { writeFile } from 'graceful-fs';
+const promiseWriteFile = promisify(writeFile);
 //TODO: Add LoggingInterceptor
 @Injectable()
 export class LoggingService {
-  async log(request: Request, context: string): Promise<void> {
-    await promiseWriteFIle('./', JSON.stringify(request), 'utf-8');
+  async logRequest(request: Request, context: string): Promise<void> {
+    const record = {
+      body: JSON.stringify(request.body),
+      headers: JSON.stringify(request.headers),
+      method: request.method,
+      hostname: `${request.hostname}${request.url}`,
+      query: JSON.stringify(request.query),
+      ip: request.ip,
+    };
+
+    await this.log(context, JSON.stringify(record));
+  }
+
+  async logResponse(response: any, context: string): Promise<void> {
+    const record =
+      typeof response === 'string' ? response : JSON.stringify(response);
+    await this.log(context, JSON.stringify(record));
+  }
+
+  private async log(context: string, message: string): Promise<void> {
+    const date = new Date().toISOString();
+    await promiseWriteFile(
+      'log',
+      `[${date}]->${context.toUpperCase()}: ${message}\n`,
+      {
+        flag: 'a',
+      }
+    );
   }
 }
