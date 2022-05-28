bc70485de626a7d5e39211a0dd7cfd667e54a998
David Rojo <davidrojo1997@gmail.com>
2022-05-28 17:02:09 +0200

    feat(libs/shared): change PartyDto signature

--- libs/shared/src/lib/domain/dto/parties/party.dto.ts
+++ libs/shared/src/lib/domain/dto/parties/party.dto.ts
@@ -1,9 +1,11 @@
 import { TripDto } from '../trips/trip.dto';
 import { MessageDto } from '../comms/message.dto';
+import { UserPartiesDto } from '../users/user-parties.dto';
 
 export class PartyDto {
   id: string;
   name: string;
   trip: TripDto;
   messages: MessageDto[];
+  users: UserPartiesDto[];
 }
