542852d95ae0592905556a5962575ef012f4e0f6
David Rojo <davidrojo1997@gmail.com>
2022-05-29 02:26:22 +0200

    build(app): migrate Nx version

--- apps/ms-images/jest.config.ts
+++ apps/ms-images/jest.config.ts
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   displayName: 'ms-images',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
--- apps/proyecto-integrado/jest.config.ts
+++ apps/proyecto-integrado/jest.config.ts
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   displayName: 'proyecto-integrado',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
