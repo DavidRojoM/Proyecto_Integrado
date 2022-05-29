e6bf8743a873b7137a4e990ae801df8ed6697cf6
David Rojo <davidrojo1997@gmail.com>
2022-05-29 03:11:23 +0200

    feat(libs/shared): add PartyDto validations

--- libs/shared/src/lib/domain/dto/parties/party.dto.ts
+++ libs/shared/src/lib/domain/dto/parties/party.dto.ts
@@ -1,11 +1,22 @@
 import { TripDto } from '../trips/trip.dto';
 import { MessageDto } from '../comms/message.dto';
 import { UserPartiesDto } from '../users/user-parties.dto';
+import { IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class PartyDto {
+  @IsString()
   id: string;
+
+  @IsString()
   name: string;
+
+  @Type(() => TripDto)
   trip: TripDto;
+
+  @IsOptional()
   messages: MessageDto[];
+
+  @IsOptional()
   users: UserPartiesDto[];
 }
