3fbf8d662b1134c7eece94b3a396f5813cee695a
David Rojo <davidrojo1997@gmail.com>
2022-05-29 23:54:39 +0200

    feat(apps/ms-images): add create event endpoint

--- apps/ms-images/src/app/app.controller.ts
+++ apps/ms-images/src/app/app.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { EventPattern } from '@nestjs/microservices';
+import {
+  ImageInput,
+  ImageUploadResponse,
+  PayloadActions,
+} from '@proyecto-integrado/shared';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @EventPattern(PayloadActions.IMAGES.CREATE)
+  create(image: ImageInput): Promise<ImageUploadResponse> {
+    return this.appService.uploadImage(image);
   }
 }
