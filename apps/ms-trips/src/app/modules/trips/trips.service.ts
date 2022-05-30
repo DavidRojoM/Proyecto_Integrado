d4fc551445a7b4a3704d5d19616ef816f0ac44ec
David Rojo <davidrojo1997@gmail.com>
2022-05-30 22:45:23 +0200

    feat(apps/ms-trips): add TripsService findAllTrips and createTrip impl

--- apps/ms-trips/src/app/modules/trips/trips.service.ts
+++ apps/ms-trips/src/app/modules/trips/trips.service.ts
@@ -1,7 +1,52 @@
 import { Injectable } from '@nestjs/common';
 import { TripsRepository } from './trips.repository';
+import {
+  FindAllTripsResponse,
+  InsertTripResponse,
+  Trip,
+  TripDto,
+} from '@proyecto-integrado/shared';
+import { Payload } from '@nestjs/microservices';
 
 @Injectable()
 export class TripsService {
   constructor(private readonly tripsRepository: TripsRepository) {}
+
+  async findAll(): Promise<FindAllTripsResponse> {
+    try {
+      const models = await this.tripsRepository.findAllTrips();
+      return {
+        ok: true,
+        value: models.map((model) => Trip.modelToDto(model)),
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: e.response.statusCode,
+          statusText: e.response.statusText,
+        },
+      };
+    }
+  }
+
+  async create(@Payload() trip: TripDto): Promise<InsertTripResponse> {
+    try {
+      const model = await this.tripsRepository.createTrip(
+        Trip.dtoToModel(trip)
+      );
+      return {
+        ok: true,
+        value: Trip.modelToDto(model),
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: e.response.statusCode,
+          statusText: e.response.statusText,
+        },
+      };
+    }
+  }
 }
