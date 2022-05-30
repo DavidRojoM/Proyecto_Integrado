e6c94c0aaee1bdc46a67a5d707d4634bc128496c
David Rojo <davidrojo1997@gmail.com>
2022-05-30 22:45:16 +0200

    feat(apps/ms-trips): add TripsRepository findAllTrips and createTrip impl

--- apps/ms-trips/src/app/modules/trips/trips.repository.ts
+++ apps/ms-trips/src/app/modules/trips/trips.repository.ts
@@ -1,5 +1,32 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { TripEntity } from '@proyecto-integrado/shared';
+import { Trip, TripEntity } from '@proyecto-integrado/shared';
+import { BadRequestException } from '@nestjs/common';
 
 @EntityRepository(TripEntity)
-export class TripsRepository extends Repository<TripEntity> {}
+export class TripsRepository extends Repository<TripEntity> {
+  async findAllTrips(): Promise<Trip[]> {
+    const result = await this.createQueryBuilder('trip')
+      .leftJoinAndSelect('trip.destination', 'destination')
+      .leftJoinAndSelect('trip.hotel', 'hotel')
+      .leftJoinAndSelect('trip.transport', 'transport')
+      .getMany();
+
+    return result.map((trip) => Trip.entityToModel(trip));
+  }
+
+  async createTrip(trip: Trip): Promise<Trip> {
+    const entity = Trip.modelToEntity(trip);
+
+    let result;
+    try {
+      result = await this.save(entity);
+    } catch (e) {
+      throw new BadRequestException({
+        statusCode: 400,
+        statusText: 'Unable to insert trip',
+      });
+    }
+
+    return Trip.entityToModel(result);
+  }
+}
