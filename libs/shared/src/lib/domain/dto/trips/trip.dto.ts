8a6d94bd3b18f0bc8c43921bee2b3363ccc0ac1e
David Rojo <davidrojo1997@gmail.com>
2022-05-31 00:09:11 +0200

    feat(libs/shared): add TripDto validations

--- libs/shared/src/lib/domain/dto/trips/trip.dto.ts
+++ libs/shared/src/lib/domain/dto/trips/trip.dto.ts
@@ -1,12 +1,40 @@
 import { DestinationDto } from './destination.dto';
 import { HotelDto } from './hotel.dto';
 import { TransportDto } from './transport.dto';
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class TripDto {
+  @IsNotEmpty()
+  @IsString()
   id: string;
+
+  @IsNotEmpty()
+  @IsString()
   from: Date;
+
+  @IsNotEmpty()
+  @IsString()
   to: Date;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => DestinationDto)
   destination: DestinationDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => HotelDto)
   hotel: HotelDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => TransportDto)
   transport: TransportDto;
 }
