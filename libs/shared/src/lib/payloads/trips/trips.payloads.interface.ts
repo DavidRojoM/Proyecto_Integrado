ca9a1822ecfcf43bc45ef9f674ad4575a783f9b3
David Rojo <davidrojo1997@gmail.com>
2022-05-31 23:50:50 +0200

    feat(libs/shared): add trip payloads

--- libs/shared/src/lib/payloads/trips/trips.payloads.interface.ts
+++ libs/shared/src/lib/payloads/trips/trips.payloads.interface.ts
@@ -1,5 +1,9 @@
 import {
+  Destination,
+  DestinationDto,
   ErrorPayload,
+  Hotel,
+  HotelDto,
   Result,
   Transport,
   TransportDto,
@@ -9,9 +13,18 @@ import {
 
 export type FindAllTripsResponse = Result<TripDto[], ErrorPayload>;
 export type FindAllTransportsResponse = Result<TransportDto[], ErrorPayload>;
+export type FindAllHotelsResponse = Result<HotelDto[], ErrorPayload>;
+export type FindAllDestinationsResponse = Result<
+  DestinationDto[],
+  ErrorPayload
+>;
 
 export type InsertTripResponse = Result<TripDto, ErrorPayload>;
 export type InsertTransportResponse = Result<TransportDto, ErrorPayload>;
+export type InsertHotelResponse = Result<HotelDto, ErrorPayload>;
+export type InsertDestinationResponse = Result<DestinationDto, ErrorPayload>;
 
 export type InsertTrip = Result<Trip, ErrorPayload>;
 export type InsertTransport = Result<Transport, ErrorPayload>;
+export type InsertHotel = Result<Hotel, ErrorPayload>;
+export type InsertDestination = Result<Destination, ErrorPayload>;
