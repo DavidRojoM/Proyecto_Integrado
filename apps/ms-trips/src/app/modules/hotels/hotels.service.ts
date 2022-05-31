3d91e630786fd343a261b6272899c2fd691cc317
David Rojo <davidrojo1997@gmail.com>
2022-05-31 23:51:22 +0200

    feat(apps/ms-trips): add HotelsService impl

--- apps/ms-trips/src/app/modules/hotels/hotels.service.ts
+++ apps/ms-trips/src/app/modules/hotels/hotels.service.ts
@@ -1,7 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { HotelsRepository } from './hotels.repository';
+import {
+  FindAllHotelsResponse,
+  Hotel,
+  HotelDto,
+  InsertHotelResponse,
+} from '@proyecto-integrado/shared';
+import { Payload } from '@nestjs/microservices';
 
 @Injectable()
 export class HotelsService {
   constructor(private readonly hotelsRepository: HotelsRepository) {}
+
+  async findAll(): Promise<FindAllHotelsResponse> {
+    try {
+      const hotels = await this.hotelsRepository.findAllHotels();
+      return {
+        ok: true,
+        value: hotels.map((hotel) => Hotel.modelToDto(hotel)),
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: e.response.statusCode,
+          statusText: e.response.statusText,
+        },
+      };
+    }
+  }
+
+  async create(@Payload() hotel: HotelDto): Promise<InsertHotelResponse> {
+    const insertResult = await this.hotelsRepository.createHotel(
+      Hotel.dtoToModel(hotel)
+    );
+
+    if (!insertResult.ok) {
+      return insertResult;
+    }
+
+    return {
+      ok: true,
+      value: Hotel.modelToDto(insertResult.value),
+    };
+  }
 }
