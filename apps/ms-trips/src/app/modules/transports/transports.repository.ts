fc22c63dd233471d4ce78f045bcd7268bb1f5467
David Rojo <davidrojo1997@gmail.com>
2022-05-31 23:18:01 +0200

    refactor(apps/ms-trips): Add TransportsRepository impl

--- apps/ms-trips/src/app/modules/transports/transports.repository.ts
+++ apps/ms-trips/src/app/modules/transports/transports.repository.ts
@@ -1,5 +1,36 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { TransportEntity } from '@proyecto-integrado/shared';
+import {
+  InsertTransport,
+  Transport,
+  TransportEntity,
+} from '@proyecto-integrado/shared';
 
 @EntityRepository(TransportEntity)
-export class TransportsRepository extends Repository<TransportEntity> {}
+export class TransportsRepository extends Repository<TransportEntity> {
+  async findAllTransports(): Promise<Transport[]> {
+    const result = await this.createQueryBuilder().getMany();
+
+    return result.map((trip) => Transport.entityToModel(trip));
+  }
+
+  async createTransport(transport: Transport): Promise<InsertTransport> {
+    const entity = Transport.modelToEntity(transport);
+    let insertedEntity;
+    try {
+      insertedEntity = await this.save(entity);
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: 400,
+          statusText: 'Could not create transport',
+        },
+      };
+    }
+
+    return {
+      ok: true,
+      value: Transport.entityToModel(insertedEntity),
+    };
+  }
+}
