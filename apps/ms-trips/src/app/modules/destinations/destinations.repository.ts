580d8be00fc36398bd2d48de8f3ed1e3bf66231c
David Rojo <davidrojo1997@gmail.com>
2022-05-31 23:51:47 +0200

    feat(apps/ms-trips): add DestinationsRepository impl

--- apps/ms-trips/src/app/modules/destinations/destinations.repository.ts
+++ apps/ms-trips/src/app/modules/destinations/destinations.repository.ts
@@ -1,5 +1,38 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { DestinationEntity } from '@proyecto-integrado/shared';
+import {
+  Destination,
+  DestinationEntity,
+  InsertDestination,
+} from '@proyecto-integrado/shared';
 
 @EntityRepository(DestinationEntity)
-export class DestinationsRepository extends Repository<DestinationEntity> {}
+export class DestinationsRepository extends Repository<DestinationEntity> {
+  async findAllDestinations(): Promise<Destination[]> {
+    const result = await this.createQueryBuilder().getMany();
+
+    return result.map((destination) => Destination.entityToModel(destination));
+  }
+
+  async createDestination(
+    destination: Destination
+  ): Promise<InsertDestination> {
+    const entity = Destination.modelToEntity(destination);
+    let insertedEntity;
+    try {
+      insertedEntity = await this.save(entity);
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: 400,
+          statusText: 'Could not create destination',
+        },
+      };
+    }
+
+    return {
+      ok: true,
+      value: Destination.entityToModel(insertedEntity),
+    };
+  }
+}
