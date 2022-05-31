7b21b66db9e4040752a2a2a407444e5db277e274
David Rojo <davidrojo1997@gmail.com>
2022-05-31 23:18:18 +0200

    refactor(apps/ms-trips): Add TransportsController endpoints

--- apps/ms-trips/src/app/modules/transports/transports.controller.ts
+++ apps/ms-trips/src/app/modules/transports/transports.controller.ts
@@ -1,4 +1,24 @@
 import { Controller } from '@nestjs/common';
+import { TransportsService } from './transports.service';
+import { MessagePattern } from '@nestjs/microservices';
+import {
+  FindAllTransportsResponse,
+  InsertTransportResponse,
+  PayloadActions,
+  TransportDto,
+} from '@proyecto-integrado/shared';
 
-@Controller('transports')
-export class TransportsController {}
+@Controller()
+export class TransportsController {
+  constructor(private readonly transportsService: TransportsService) {}
+
+  @MessagePattern(PayloadActions.TRIPS.TRANSPORTS.FIND_ALL)
+  findAll(): Promise<FindAllTransportsResponse> {
+    return this.transportsService.findAll();
+  }
+
+  @MessagePattern(PayloadActions.TRIPS.TRANSPORTS.CREATE)
+  create(transport: TransportDto): Promise<InsertTransportResponse> {
+    return this.transportsService.create(transport);
+  }
+}
