dcf4afeb873507c40fd98f8222ebfb8614c1ca3d
David Rojo <davidrojo1997@gmail.com>
2022-05-31 23:51:28 +0200

    feat(apps/ms-trips): add HotelsController impl

--- apps/ms-trips/src/app/modules/hotels/hotels.controller.ts
+++ apps/ms-trips/src/app/modules/hotels/hotels.controller.ts
@@ -1,4 +1,24 @@
 import { Controller } from '@nestjs/common';
+import { HotelsService } from './hotels.service';
+import { MessagePattern } from '@nestjs/microservices';
+import {
+  FindAllHotelsResponse,
+  HotelDto,
+  InsertHotelResponse,
+  PayloadActions,
+} from '@proyecto-integrado/shared';
 
-@Controller('hotels')
-export class HotelsController {}
+@Controller()
+export class HotelsController {
+  constructor(private readonly hotelsService: HotelsService) {}
+
+  @MessagePattern(PayloadActions.TRIPS.HOTELS.FIND_ALL)
+  findAll(): Promise<FindAllHotelsResponse> {
+    return this.hotelsService.findAll();
+  }
+
+  @MessagePattern(PayloadActions.TRIPS.HOTELS.CREATE)
+  create(hotel: HotelDto): Promise<InsertHotelResponse> {
+    return this.hotelsService.create(hotel);
+  }
+}
