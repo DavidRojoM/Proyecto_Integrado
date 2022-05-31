8e93ea7d95eaa5e23d40af0a00339cc78a4fda0e
David Rojo <davidrojo1997@gmail.com>
2022-05-31 23:51:53 +0200

    feat(apps/ms-trips): add DestinationsService impl

--- apps/ms-trips/src/app/modules/destinations/destinations.service.ts
+++ apps/ms-trips/src/app/modules/destinations/destinations.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { DestinationsRepository } from './destinations.repository';
+import {
+  FindAllDestinationsResponse,
+  Destination,
+  DestinationDto,
+  InsertDestinationResponse,
+} from '@proyecto-integrado/shared';
+import { Payload } from '@nestjs/microservices';
 
 @Injectable()
 export class DestinationsService {
   constructor(
     private readonly destinationsRepository: DestinationsRepository
   ) {}
+
+  async findAll(): Promise<FindAllDestinationsResponse> {
+    try {
+      const destinations =
+        await this.destinationsRepository.findAllDestinations();
+      return {
+        ok: true,
+        value: destinations.map((destination) =>
+          Destination.modelToDto(destination)
+        ),
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: e.response.statusCode,
+          statusText: e.response.statusText,
+        },
+      };
+    }
+  }
+
+  async create(
+    @Payload() destination: DestinationDto
+  ): Promise<InsertDestinationResponse> {
+    const insertResult = await this.destinationsRepository.createDestination(
+      Destination.dtoToModel(destination)
+    );
+
+    if (!insertResult.ok) {
+      return insertResult;
+    }
+
+    return {
+      ok: true,
+      value: Destination.modelToDto(insertResult.value),
+    };
+  }
 }
