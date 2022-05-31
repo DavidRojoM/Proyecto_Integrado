d6bb3135c61ddef07a71d83b6b81510473bda34f
David Rojo <davidrojo1997@gmail.com>
2022-05-31 23:17:15 +0200

    feat(libs/shared): add Transports payloads

--- libs/shared/src/lib/payloads/trips/trips.payloads.interface.ts
+++ libs/shared/src/lib/payloads/trips/trips.payloads.interface.ts
@@ -1,6 +1,17 @@
-import { ErrorPayload, Result, TripDto } from '@proyecto-integrado/shared';
+import {
+  ErrorPayload,
+  Result,
+  Transport,
+  TransportDto,
+  Trip,
+  TripDto,
+} from '@proyecto-integrado/shared';
 
 export type FindAllTripsResponse = Result<TripDto[], ErrorPayload>;
+export type FindAllTransportsResponse = Result<TransportDto[], ErrorPayload>;
 
 export type InsertTripResponse = Result<TripDto, ErrorPayload>;
-export type InsertTrip = Result<TripDto, ErrorPayload>;
+export type InsertTransportResponse = Result<TransportDto, ErrorPayload>;
+
+export type InsertTrip = Result<Trip, ErrorPayload>;
+export type InsertTransport = Result<Transport, ErrorPayload>;
