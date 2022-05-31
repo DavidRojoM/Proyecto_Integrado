7c2ab51adc2176816bb3d4d7ae36ebbfe2645413
David Rojo <davidrojo1997@gmail.com>
2022-05-31 22:15:59 +0200

    feat(libs/shared): add types

--- libs/shared/src/lib/payloads/trips/trips.payloads.interface.ts
+++ libs/shared/src/lib/payloads/trips/trips.payloads.interface.ts
@@ -3,3 +3,4 @@ import { ErrorPayload, Result, TripDto } from '@proyecto-integrado/shared';
 export type FindAllTripsResponse = Result<TripDto[], ErrorPayload>;
 
 export type InsertTripResponse = Result<TripDto, ErrorPayload>;
+export type InsertTrip = Result<TripDto, ErrorPayload>;
--- libs/shared/src/lib/payloads/users/users.payloads.interface.ts
+++ libs/shared/src/lib/payloads/users/users.payloads.interface.ts
@@ -1,11 +1,14 @@
 import { UserDto } from '../../domain/dto/users/user.dto';
 import { ErrorPayload } from '../errors/error.payload';
 import { Result } from '../../domain/types/result.type';
+import { User } from '../../domain/models/users/user.model';
 
 export interface FindOneByUsername {
   username: string;
 }
 
 export type FindUserByUsernameResponse = Result<UserDto, ErrorPayload>;
+export type FindUserByUsername = Result<User, ErrorPayload>;
 
 export type AddUserResponse = Result<UserDto, ErrorPayload>;
+export type InsertUser = Result<User, ErrorPayload>;
