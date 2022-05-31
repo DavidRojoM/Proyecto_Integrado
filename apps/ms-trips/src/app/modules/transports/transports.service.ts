9d0f6b1d23ee7b7c44074f6f4514ae37892966b6
David Rojo <davidrojo1997@gmail.com>
2022-05-31 23:18:09 +0200

    refactor(apps/ms-trips): Add TransportsService impl

--- apps/ms-trips/src/app/modules/transports/transports.service.ts
+++ apps/ms-trips/src/app/modules/transports/transports.service.ts
@@ -1,7 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { TransportsRepository } from './transports.repository';
+import {
+  FindAllTransportsResponse,
+  InsertTransportResponse,
+  Transport,
+  TransportDto,
+} from '@proyecto-integrado/shared';
+import { Payload } from '@nestjs/microservices';
 
 @Injectable()
 export class TransportsService {
   constructor(private readonly transportsRepository: TransportsRepository) {}
+
+  async findAll(): Promise<FindAllTransportsResponse> {
+    try {
+      const transports = await this.transportsRepository.findAllTransports();
+      return {
+        ok: true,
+        value: transports.map((transport) => Transport.modelToDto(transport)),
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: e.response.statusCode,
+          statusText: e.response.statusText,
+        },
+      };
+    }
+  }
+
+  async create(
+    @Payload() transport: TransportDto
+  ): Promise<InsertTransportResponse> {
+    const insertResult = await this.transportsRepository.createTransport(
+      Transport.dtoToModel(transport)
+    );
+
+    if (!insertResult.ok) {
+      return insertResult;
+    }
+
+    return {
+      ok: true,
+      value: Transport.modelToDto(insertResult.value),
+    };
+  }
 }
