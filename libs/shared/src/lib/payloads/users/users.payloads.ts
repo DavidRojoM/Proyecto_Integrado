36482534475201642da39a044b160078335bc0ae
David Rojo <davidrojo1997@gmail.com>
2022-06-01 23:34:24 +0200

    feat(libs/shared): add UserParty payloads

--- libs/shared/src/lib/payloads/users/users.payloads.ts
+++ libs/shared/src/lib/payloads/users/users.payloads.ts
@@ -2,6 +2,8 @@ import { UserDto } from '../../domain/dto/users/user.dto';
 import { ErrorPayload } from '../errors/error.payloads';
 import { Result } from '../../domain/types/result.type';
 import { User } from '../../domain/models/users/user.model';
+import { UserPartyDto } from '../../domain/dto/users/user-party.dto';
+import { UserParty } from '../../domain/models/users/user-party.model';
 
 export interface FindOneByUsername {
   username: string;
@@ -12,3 +14,9 @@ export type FindUserByUsername = Result<User, ErrorPayload>;
 
 export type AddUserResponse = Result<UserDto, ErrorPayload>;
 export type InsertUser = Result<User, ErrorPayload>;
+
+export type FindUserPartyByIdResponse = Result<UserPartyDto, ErrorPayload>;
+export type InsertUserPartyResponse = Result<UserPartyDto, ErrorPayload>;
+
+export type FindUserPartyById = Result<UserParty, ErrorPayload>;
+export type InsertUserParty = Result<UserParty, ErrorPayload>;
