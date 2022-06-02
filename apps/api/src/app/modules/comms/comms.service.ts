8302e8292f9a90a8f7cbcca4dc1363557f437ae5
David Rojo <davidrojo1997@gmail.com>
2022-06-03 01:08:00 +0200

    feat(apps/api): add CommsService

---
+++ apps/api/src/app/modules/comms/comms.service.ts
@@ -0,0 +1,12 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { MessageDto, PayloadActions } from '@proyecto-integrado/shared';
+
+@Injectable()
+export class CommsService {
+  constructor(@Inject() private readonly commsProxy: ClientProxy) {}
+
+  sendMessage(message: MessageDto): void {
+    this.commsProxy.emit(PayloadActions.COMMS.SEND_MESSAGE, message);
+  }
+}
