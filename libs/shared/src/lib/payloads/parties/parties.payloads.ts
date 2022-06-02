0741df7dfe5fd8cddf8a5ac25a22ace7bea20fbd
David Rojo <davidrojo1997@gmail.com>
2022-06-03 00:59:50 +0200

    feat(libs/shared): add parties payloads

--- libs/shared/src/lib/payloads/parties/parties.payloads.ts
+++ libs/shared/src/lib/payloads/parties/parties.payloads.ts
@@ -8,7 +8,7 @@ import {
 } from '@proyecto-integrado/shared';
 
 export type FindAllPartiesResponse = Result<PartyDto[], ErrorPayload>;
-export type FindPartyByIdResponse = Result<PartyDto, ErrorPayload>;
+export type FindPartyResponse = Result<PartyDto, ErrorPayload>;
 export type InsertPartyResponse = Result<PartyDto, ErrorPayload>;
 export type UpdatePartyResponse = Result<PartyDto, ErrorPayload>;
 export type DeletePartyResponse = Result<PartyDto, ErrorPayload>;
@@ -20,3 +20,7 @@ export type InsertParty = Result<Party, ErrorPayload>;
 export type UpdateParty = Result<Party, ErrorPayload>;
 export type DeleteParty = Result<Party, ErrorPayload>;
 export type JoinParty = Result<UserParty, ErrorPayload>;
+
+export interface FindPartyByIdPayload {
+  id: string;
+}
