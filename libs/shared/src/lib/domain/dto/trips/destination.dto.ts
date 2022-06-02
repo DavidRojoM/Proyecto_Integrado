6f512388011a3256ab8a2097cd332efb7f733e56
David Rojo <davidrojo1997@gmail.com>
2022-06-03 01:02:06 +0200

    feat(libs/shared): add DestinationDto validations

--- libs/shared/src/lib/domain/dto/trips/destination.dto.ts
+++ libs/shared/src/lib/domain/dto/trips/destination.dto.ts
@@ -1,5 +1,13 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class DestinationDto {
+  @IsOptional()
+  @IsNumber()
   id?: number;
+
+  @IsString()
   name: string;
+
+  @IsString()
   description: string;
 }
