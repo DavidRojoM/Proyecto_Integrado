2c884e63f484ab3640195f01aebb3b624c2b0bd6
David Rojo <davidrojo1997@gmail.com>
2022-06-03 01:02:25 +0200

    feat(libs/shared): add TransportDto validations

--- libs/shared/src/lib/domain/dto/trips/transport.dto.ts
+++ libs/shared/src/lib/domain/dto/trips/transport.dto.ts
@@ -1,9 +1,20 @@
 import { Transports } from '../../enums/transports.enum';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class TransportDto {
+  @IsOptional()
+  @IsNumber()
   id: number;
+
+  @IsString()
   name: string;
+
+  @IsEnum(Transports)
   type: Transports;
+
+  @IsNumber()
   price: number;
+
+  @IsString()
   brand: string;
 }
