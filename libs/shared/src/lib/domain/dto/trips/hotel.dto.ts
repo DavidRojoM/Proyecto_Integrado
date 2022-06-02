dd93d7739d31410c3206c7f866229f5fe99ec52d
David Rojo <davidrojo1997@gmail.com>
2022-06-03 01:02:17 +0200

    feat(libs/shared): add HotelDto validations

--- libs/shared/src/lib/domain/dto/trips/hotel.dto.ts
+++ libs/shared/src/lib/domain/dto/trips/hotel.dto.ts
@@ -1,8 +1,22 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class HotelDto {
+  @IsOptional()
+  @IsNumber()
   id: number;
+
+  @IsString()
   name: string;
+
+  @IsString()
   address: string;
+
+  @IsString()
   phone: string;
+
+  @IsNumber()
   nightPrice: number;
+
+  @IsString()
   image: string;
 }
