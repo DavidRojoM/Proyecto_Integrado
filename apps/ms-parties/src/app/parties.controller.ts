2b9ec78e8cd40ab1ad235b52cc61c3e34d79150b
David Rojo <davidrojo1997@gmail.com>
2022-06-03 23:51:03 +0200

    feat(apps/ms-parties): add PartiesController endpoints

--- apps/ms-parties/src/app/parties.controller.ts
+++ apps/ms-parties/src/app/parties.controller.ts
@@ -1,10 +1,14 @@
 import { Controller } from '@nestjs/common';
 
 import { PartiesService } from './parties.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import {
+  FindAllPartiesResponse,
+  FindPartyResponse,
+  InsertPartyResponse,
   InsertUserPartyResponse,
   JoinPartyDto,
+  PartyDto,
   PayloadActions,
 } from '@proyecto-integrado/shared';
 
@@ -13,7 +17,22 @@ export class PartiesController {
   constructor(private readonly partiesService: PartiesService) {}
 
   @MessagePattern(PayloadActions.PARTIES.JOIN)
-  async joinParty(data: JoinPartyDto): Promise<InsertUserPartyResponse> {
+  joinParty(data: JoinPartyDto): Promise<InsertUserPartyResponse> {
     return this.partiesService.joinParty(data);
+  }
+
+  @MessagePattern(PayloadActions.PARTIES.FIND_BY_ID)
+  async findById(@Payload('id') id: string): Promise<FindPartyResponse> {
+    return this.partiesService.findById(id);
+  }
+
+  @MessagePattern(PayloadActions.PARTIES.CREATE)
+  async create(@Payload() party: PartyDto): Promise<InsertPartyResponse> {
+    return this.partiesService.create(party);
+  }
+
+  @MessagePattern(PayloadActions.PARTIES.FIND_ALL)
+  async findAll(): Promise<FindAllPartiesResponse> {
+    return this.partiesService.findAll();
   }
 }
