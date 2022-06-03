01e42d1a3d2586fef40bf1fae03a854ca1f434a8
David Rojo <davidrojo1997@gmail.com>
2022-06-03 23:49:14 +0200

    feat(apps/ms-comms): add CommsService#sendMessage endp

--- apps/ms-comms/src/app/comms.controller.ts
+++ apps/ms-comms/src/app/comms.controller.ts
@@ -1,15 +1,19 @@
 import { Controller } from '@nestjs/common';
 
 import { CommsService } from './comms.service';
-import { EventPattern } from '@nestjs/microservices';
-import { MessageDto, PayloadActions } from '@proyecto-integrado/shared';
+import { MessagePattern } from '@nestjs/microservices';
+import {
+  MessageDto,
+  PayloadActions,
+  SendMessageResponse,
+} from '@proyecto-integrado/shared';
 
 @Controller()
 export class CommsController {
   constructor(private readonly commsService: CommsService) {}
 
-  @EventPattern(PayloadActions.COMMS.SEND_MESSAGE)
-  sendMessage(message: MessageDto): void {
-    this.commsService.insertMessage(message);
+  @MessagePattern(PayloadActions.COMMS.SEND_MESSAGE)
+  sendMessage(message: MessageDto): Promise<SendMessageResponse> {
+    return this.commsService.insertMessage(message);
   }
 }
