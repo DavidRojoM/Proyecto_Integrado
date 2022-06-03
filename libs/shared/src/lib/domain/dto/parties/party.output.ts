9850814daf896bd9848bd2f1154ef6aa41f604d4
David Rojo <davidrojo1997@gmail.com>
2022-06-03 23:44:06 +0200

    feat(libs/shared): add PartyOutput

---
+++ libs/shared/src/lib/domain/dto/parties/party.output.ts
@@ -0,0 +1,13 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class PartyOutput {
+  @Field()
+  id: string;
+
+  @Field()
+  username: string;
+
+  @Field()
+  image: string;
+}
