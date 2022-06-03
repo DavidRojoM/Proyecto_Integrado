2b2ac3aec5bf1d7da8ce912d679f2d7d2e595906
David Rojo <davidrojo1997@gmail.com>
2022-06-03 23:47:47 +0200

    feat(libs/shared): add PartiesRepository impl

--- libs/shared/src/lib/database/repositories/parties.repository.ts
+++ libs/shared/src/lib/database/repositories/parties.repository.ts
@@ -1,4 +1,10 @@
-import { InsertParty, PartyEntity, Party } from '@proyecto-integrado/shared';
+import {
+  InsertParty,
+  PartyEntity,
+  Party,
+  FindPartyById,
+  FindAllParties,
+} from '@proyecto-integrado/shared';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(PartyEntity)
@@ -9,24 +15,66 @@ export class PartiesRepository extends Repository<PartyEntity> {
     return result.map((userParty) => Party.entityToModel(userParty));
   }
 
-  async createParty(party: Party): Promise<InsertParty> {
-    const entity = Party.modelToEntity(party);
-    let insertedEntity;
-    try {
-      insertedEntity = await this.save(entity);
-    } catch (e) {
+  async findById(id: string): Promise<FindPartyById> {
+    const entity = await this.findOne(id);
+
+    if (!entity) {
       return {
         ok: false,
         error: {
-          statusCode: e.statusCode,
-          statusText: e.statusText,
+          statusCode: 404,
+          statusText: 'Not Found',
         },
       };
     }
 
     return {
       ok: true,
-      value: Party.entityToModel(insertedEntity),
+      value: Party.entityToModel(entity),
+    };
+  }
+
+  async createParty(party: Party): Promise<InsertParty> {
+    const entity = Party.modelToEntity(party);
+    try {
+      await this.insert(entity);
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: e.errno,
+          statusText: e.sqlMessage,
+        },
+      };
+    }
+
+    return {
+      ok: true,
+      value: Party.entityToModel(entity),
+    };
+  }
+
+  async findAll(): Promise<FindAllParties> {
+    let result;
+    try {
+      result = await this.createQueryBuilder('party')
+        .leftJoinAndSelect('party.trip', 'trip')
+        .leftJoinAndSelect('party.userParties', 'userParties')
+        .leftJoinAndSelect('userParties.user', 'user')
+        .getMany();
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: e.errno,
+          statusText: e.sqlMessage,
+        },
+      };
+    }
+
+    return {
+      ok: true,
+      value: result.map((party) => Party.entityToModel(party)),
     };
   }
 }
