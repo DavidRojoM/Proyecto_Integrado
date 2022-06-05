6f21dd833522fd79e948198eaf7147d31a05578a
David Rojo <davidrojo1997@gmail.com>
2022-06-05 23:16:23 +0200

    feat(apps/front): add AppModule first routes

---
+++ apps/proyecto-integrado/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./core/pages/landing/landing.module').then(
+        (m) => m.LandingModule
+      ),
+  },
+  {
+    path: 'notfound',
+    loadChildren: () =>
+      import('./core/pages/not-found/not-found.module').then(
+        (m) => m.NotFoundModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'notfound',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- apps/proyecto-integrado/src/app/app.module.ts
+++ apps/proyecto-integrado/src/app/app.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
