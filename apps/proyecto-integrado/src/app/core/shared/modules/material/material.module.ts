31239a64348011b4fe471ab59c1c55eef14d37a5
David Rojo <davidrojo1997@gmail.com>
2022-06-07 23:55:38 +0200

    feat(apps/front): add MaterialModule

---
+++ apps/proyecto-integrado/src/app/core/shared/modules/material/material.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [
+    MatTabsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatStepperModule,
+    MatAutocompleteModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+  ],
+})
+export class MaterialModule {}
