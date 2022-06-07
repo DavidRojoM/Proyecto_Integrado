dae461b99e712cc6cf0a8c93cf83ce202009aaf6
David Rojo <davidrojo1997@gmail.com>
2022-06-07 23:57:29 +0200

    feat(apps/front): add LandingComponent

--- apps/proyecto-integrado/src/app/core/pages/landing/landing.module.ts
+++ apps/proyecto-integrado/src/app/core/pages/landing/landing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
+import { MaterialModule } from '../../shared/modules/material/material.module';
 
 @NgModule({
   declarations: [LandingComponent],
-  imports: [CommonModule, LandingRoutingModule],
+  imports: [CommonModule, LandingRoutingModule, MaterialModule],
 })
 export class LandingModule {}
