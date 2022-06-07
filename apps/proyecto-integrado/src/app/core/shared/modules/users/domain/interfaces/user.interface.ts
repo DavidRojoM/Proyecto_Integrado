a6e25fb9baf0137b34efdf49c19065c39c70c00a
David Rojo <davidrojo1997@gmail.com>
2022-06-07 23:54:56 +0200

    feat(apps/front): add User interface

---
+++ apps/proyecto-integrado/src/app/core/shared/modules/users/domain/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+  nationality: string;
+  bankAccount: string;
+  image: string;
+  role: string;
+}
