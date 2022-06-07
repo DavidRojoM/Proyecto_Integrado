c3a3013d83529659ee78c16f8b1d03669c053328
David Rojo <davidrojo1997@gmail.com>
2022-06-07 23:56:53 +0200

    feat(apps/front): add LoginComponent impl

--- apps/proyecto-integrado/src/app/core/pages/auth/login/login.component.ts
+++ apps/proyecto-integrado/src/app/core/pages/auth/login/login.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'proyecto-integrado-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent {
+  @Output() loginEmitter = new EventEmitter();
 
-  ngOnInit(): void {}
+  hidePassword = true;
+
+  form = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  login() {
+    if (this.form.valid) {
+      this.loginEmitter.emit(this.form.value);
+    }
+  }
 }
