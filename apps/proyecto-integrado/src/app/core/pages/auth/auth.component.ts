16d5e55c1ba6a6d513a3dd6d264ce5f35f57a40d
David Rojo <davidrojo1997@gmail.com>
2022-06-07 23:57:16 +0200

    feat(apps/front): add AuthComponent impl

--- apps/proyecto-integrado/src/app/core/pages/auth/auth.component.ts
+++ apps/proyecto-integrado/src/app/core/pages/auth/auth.component.ts
@@ -1,12 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { UsersService } from '../../shared/modules/users/services/users.service';
+import { User } from '../../shared/modules/users/domain/interfaces/user.interface';
+import { Credentials } from '../../shared/modules/auth/domain/credentials.interface';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../../shared/services/local-storage.service';
+import { MatTabGroup } from '@angular/material/tabs';
 
 @Component({
   selector: 'proyecto-integrado-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent implements OnInit {
-  constructor() {}
+export class AuthComponent {
+  @ViewChild('tabs') tabGroup!: MatTabGroup;
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly router: Router,
+    private readonly localStorageService: LocalStorageService
+  ) {}
 
-  ngOnInit(): void {}
+  signup(user: User) {
+    this.usersService.signup(user).subscribe(() => {
+      this.tabGroup.selectedIndex = 0;
+    });
+  }
+
+  login(credentials: Credentials) {
+    this.usersService.login(credentials).subscribe((res) => {
+      if (res.access_token) {
+        this.localStorageService.setItem('access_token', res.access_token);
+        this.localStorageService.setItem('userId', res.user.id);
+        this.localStorageService.setItem('role', res.user.role);
+        this.localStorageService.setItem('username', res.user.username);
+        this.router.navigate(['/home']);
+      }
+    });
+  }
+
+  logout() {
+    this.localStorageService.removeItem('access_token');
+    this.localStorageService.removeItem('userId');
+    this.localStorageService.removeItem('role');
+    this.localStorageService.removeItem('username');
+    this.router.navigate(['/']);
+  }
 }
--- apps/proyecto-integrado/src/app/core/pages/auth/auth.module.ts
+++ apps/proyecto-integrado/src/app/core/pages/auth/auth.module.ts
@@ -3,11 +3,28 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AuthComponent } from './auth.component';
-import { MatTabsModule } from '@angular/material/tabs';
 import { AuthRoutingModule } from './auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { NgxFileDragDropModule } from 'ngx-file-drag-drop';
+import { SharedModule } from '../../shared/shared.module';
+import { MaterialModule } from '../../shared/modules/material/material.module';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, SignupComponent],
-  imports: [CommonModule, MatTabsModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    NgxFileDragDropModule,
+    MaterialModule,
+  ],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    },
+  ],
 })
 export class AuthModule {}
