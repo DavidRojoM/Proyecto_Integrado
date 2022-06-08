95ca259523662d060ae7d8d59d1730c34c2732ab
David Rojo <davidrojo1997@gmail.com>
2022-06-08 23:53:16 +0200

    feat(apps/front): add PartiesActionTypes

---
+++ apps/proyecto-integrado/src/app/state/actions/parties/parties-action.types.enum.ts
@@ -0,0 +1,25 @@
+export enum PartiesActionTypes {
+  CREATE_PARTY_REQUEST = '[Parties] Create Party Request',
+  CREATE_PARTY_SUCCESS = '[Parties] Create Party Success',
+  CREATE_PARTY_FAILURE = '[Parties] Create Party Failure',
+
+  GET_PARTIES_REQUEST = '[Parties] Get Parties Request',
+  GET_PARTIES_SUCCESS = '[Parties] Get Parties Success',
+  GET_PARTIES_FAILURE = '[Parties] Get Parties Failure',
+
+  DELETE_PARTY_REQUEST = '[Parties] Delete Party Request',
+  DELETE_PARTY_SUCCESS = '[Parties] Delete Party Success',
+  DELETE_PARTY_FAILURE = '[Parties] Delete Party Failure',
+
+  UPDATE_PARTY_REQUEST = '[Parties] Update Party Request',
+  UPDATE_PARTY_SUCCESS = '[Parties] Update Party Success',
+  UPDATE_PARTY_FAILURE = '[Parties] Update Party Failure',
+
+  JOIN_PARTY_REQUEST = '[Parties] Join Party Request',
+  JOIN_PARTY_SUCCESS = '[Parties] Join Party Success',
+  JOIN_PARTY_FAILURE = '[Parties] Join Party Failure',
+
+  LEAVE_PARTY_REQUEST = '[Parties] Leave Party Request',
+  LEAVE_PARTY_SUCCESS = '[Parties] Leave Party Success',
+  LEAVE_PARTY_FAILURE = '[Parties] Leave Party Failure',
+}
