29fb901b2c88f013546bce28a0a3347f3cade6f9
David Rojo <davidrojo1997@gmail.com>
2022-06-08 23:53:05 +0200

    feat(apps/front): add CommsActionTypes

---
+++ apps/proyecto-integrado/src/app/state/actions/comms/comms-action.types.enum.ts
@@ -0,0 +1,9 @@
+export enum CommsActionTypes {
+  LOAD_MESSAGES_REQUEST = '[Comms] Load Messages Request',
+  LOAD_MESSAGES_SUCCESS = '[Comms] Load Messages Success',
+  LOAD_MESSAGES_FAILURE = '[Comms] Load Messages Failure',
+
+  LOAD_MESSAGE_SUCCESS = '[Comms] Load Message Success',
+
+  SEND_MESSAGE_REQUEST = '[Comms] Send Message Request',
+}
