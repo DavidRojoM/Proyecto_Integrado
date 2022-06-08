56329ee0070fad921ac006517df9c269c9bb6cfe
David Rojo <davidrojo1997@gmail.com>
2022-06-08 22:25:39 +0200

    feat(apps/front): add Wishlists page

---
+++ apps/proyecto-integrado/src/app/core/pages/wishlists/wishlists.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'proyecto-integrado-wishlists',
+  templateUrl: './wishlists.component.html',
+  styleUrls: ['./wishlists.component.scss'],
+})
+export class WishlistsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
