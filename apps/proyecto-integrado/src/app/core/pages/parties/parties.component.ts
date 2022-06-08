f1f6c722cf1a6ac92424d8dd0643c744dcff4780
David Rojo <davidrojo1997@gmail.com>
2022-06-08 22:25:12 +0200

    feat(apps/front): add Parties page

---
+++ apps/proyecto-integrado/src/app/core/pages/parties/parties.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'proyecto-integrado-parties',
+  templateUrl: './parties.component.html',
+  styleUrls: ['./parties.component.scss'],
+})
+export class PartiesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
