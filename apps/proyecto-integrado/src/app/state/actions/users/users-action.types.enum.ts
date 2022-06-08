c1990edc9561165d152672ea684bffb4752a42cc
David Rojo <davidrojo1997@gmail.com>
2022-06-08 23:53:37 +0200

    feat(apps/front): add UsersActionTypes

---
+++ apps/proyecto-integrado/src/app/state/actions/users/users-action.types.enum.ts
@@ -0,0 +1,25 @@
+export enum UsersActionTypes {
+  CREATE_USER_REQUEST = '[Users] Create User Request',
+  CREATE_USER_SUCCESS = '[Users] Create User Success',
+  CREATE_USER_FAILURE = '[Users] Create User Failure',
+
+  GET_USERS_REQUEST = '[Users] Get Users Request',
+  GET_USERS_SUCCESS = '[Users] Get Users Success',
+  GET_USERS_FAILURE = '[Users] Get Users Failure',
+
+  DELETE_USER_REQUEST = '[Users] Delete User Request',
+  DELETE_USER_SUCCESS = '[Users] Delete User Success',
+  DELETE_USER_FAILURE = '[Users] Delete User Failure',
+
+  UPDATE_USER_REQUEST = '[Users] Update User Request',
+  UPDATE_USER_SUCCESS = '[Users] Update User Success',
+  UPDATE_USER_FAILURE = '[Users] Update User Failure',
+
+  GET_USER_REQUEST = '[Users] Get User Request',
+  GET_USER_SUCCESS = '[Users] Get User Success',
+  GET_USER_FAILURE = '[Users] Get User Failure',
+
+  BAN_USER_REQUEST = '[Users] Ban User Request',
+  BAN_USER_SUCCESS = '[Users] Ban User Success',
+  BAN_USER_FAILURE = '[Users] Ban User Failure',
+}
