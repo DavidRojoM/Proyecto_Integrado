851cb4e49e89b9705beca7562e91b9f597617374
David Rojo <davidrojo1997@gmail.com>
2022-06-09 23:53:35 +0200

    feat(apps/front): update AuthActions

--- apps/proyecto-integrado/src/app/state/actions/auth/auth.actions.ts
+++ apps/proyecto-integrado/src/app/state/actions/auth/auth.actions.ts
@@ -1,22 +1,45 @@
 import { createAction, props } from '@ngrx/store';
 import { AuthActionTypes } from './auth-action.types.enum';
 import { LoginResponse } from '../../../core/shared/modules/auth/domain/login-response.interface';
+import { Credentials } from '../../../core/shared/modules/auth/domain/credentials.interface';
+import { User } from '../../../core/shared/modules/users/domain/interfaces/user.interface';
 
 export const AuthActions = {
   loginRequest: createAction(
     AuthActionTypes.LOGIN_REQUEST,
-    props<{ username: string; password: string }>()
+    props<{ credentials: Credentials }>()
   ),
   loginSuccess: createAction(
     AuthActionTypes.LOGIN_SUCCESS,
-    props<LoginResponse>()
+    props<{ loginResponse: LoginResponse }>()
   ),
   loginFailure: createAction(
     AuthActionTypes.LOGIN_FAILURE,
     props<{ error: string }>()
   ),
 
-  logoutRequest: createAction(AuthActionTypes.LOGOUT_REQUEST),
+  signupRequest: createAction(
+    AuthActionTypes.SIGNUP_REQUEST,
+    props<{ user: User }>()
+  ),
 
-  logoutSuccess: createAction(AuthActionTypes.LOGOUT_SUCCESS),
+  signupSuccess: createAction(AuthActionTypes.SIGNUP_SUCCESS),
+
+  signupFailure: createAction(
+    AuthActionTypes.SIGNUP_FAILURE,
+    props<{ error: string }>()
+  ),
+
+  checkAuthRequest: createAction(AuthActionTypes.CHECK_AUTH_REQUEST),
+
+  checkAuthSuccess: createAction(
+    AuthActionTypes.CHECK_AUTH_SUCCESS,
+    props<{ loginResponse: LoginResponse }>()
+  ),
+  checkAuthFailure: createAction(
+    AuthActionTypes.CHECK_AUTH_FAILURE,
+    props<{ error: string }>()
+  ),
+
+  logoutRequest: createAction(AuthActionTypes.LOGOUT_REQUEST),
 };
