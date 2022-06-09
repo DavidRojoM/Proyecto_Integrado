d0af237a0767eae69eb04bf5fe9b427c1ece3731
David Rojo <davidrojo1997@gmail.com>
2022-06-09 23:55:07 +0200

    feat(apps/front): update StateModule

--- apps/proyecto-integrado/src/app/state/state.module.ts
+++ apps/proyecto-integrado/src/app/state/state.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StoreModule } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreService } from './store.service';
+import { AuthEffects } from './effects/auth/auth.effects';
+import { SharedModule } from '../core/shared/shared.module';
+import { AuthActions } from './actions/auth/auth.actions';
+import { ROOT_REDUCERS } from './app.state';
 
 @NgModule({
   declarations: [],
   imports: [
+    SharedModule,
     CommonModule,
-    StoreModule.forRoot([]),
-    EffectsModule.forRoot([]),
+    StoreModule.forRoot(ROOT_REDUCERS),
+    EffectsModule.forRoot([AuthEffects]),
     StoreDevtoolsModule.instrument(),
   ],
-  providers: [StoreService],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (store: Store<any>) => {
+        return () => {
+          store.dispatch(AuthActions.checkAuthRequest());
+        };
+      },
+      multi: true,
+      deps: [Store],
+    },
+  ],
 })
 export class StateModule {}
--- apps/proyecto-integrado/src/app/state/store.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class StoreService {
-  constructor(private readonly store$: Store) {}
-}
