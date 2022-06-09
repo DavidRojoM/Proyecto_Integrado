d4375e95ad330c322af3a966d51342d424f5e748
David Rojo <davidrojo1997@gmail.com>
2022-06-09 23:56:33 +0200

    feat(apps/front): update SharedModule

--- apps/proyecto-integrado/src/app/core/shared/shared.module.ts
+++ apps/proyecto-integrado/src/app/core/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { SnackbarService } from './services/snackbar.service';
 import { UsersService } from './modules/users/services/users.service';
 import { CountryService } from './modules/country/services/country.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from './modules/auth/services/auth.service';
+import { LocalStorageService } from './services/local-storage.service';
+import { PartiesService } from './modules/parties/services/parties.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  providers: [MatSnackBar, SnackbarService, UsersService, CountryService],
+  providers: [
+    MatSnackBar,
+    SnackbarService,
+    LocalStorageService,
+    UsersService,
+    CountryService,
+    AuthService,
+    PartiesService,
+  ],
 })
 export class SharedModule {}
