918ef4a8acff300af79b82176fe8fce97e3ccbc5
David Rojo <davidrojo1997@gmail.com>
2022-06-09 23:53:30 +0200

    feat(apps/front): update AuthActionTypes

--- apps/proyecto-integrado/src/app/state/actions/auth/auth-action.types.enum.ts
+++ apps/proyecto-integrado/src/app/state/actions/auth/auth-action.types.enum.ts
@@ -3,6 +3,13 @@ export enum AuthActionTypes {
   LOGIN_SUCCESS = '[Auth] Login Success',
   LOGIN_FAILURE = '[Auth] Login Failure',
 
+  SIGNUP_REQUEST = '[Auth] Signup Request',
+  SIGNUP_SUCCESS = '[Auth] Signup Success',
+  SIGNUP_FAILURE = '[Auth] Signup Failure',
+
+  CHECK_AUTH_REQUEST = '[Auth] Check Auth Request',
+  CHECK_AUTH_SUCCESS = '[Auth] Check Auth Success',
+  CHECK_AUTH_FAILURE = '[Auth] Check Auth Failure',
+
   LOGOUT_REQUEST = '[Auth] Logout Request',
-  LOGOUT_SUCCESS = '[Auth] Logout Success',
 }
