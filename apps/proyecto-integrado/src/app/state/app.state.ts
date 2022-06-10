a9bce50c04d0727e39d43cdd86341171f2abc244
David Rojo <davidrojo1997@gmail.com>
2022-06-10 22:54:54 +0200

    feat(apps/front): add parties to AppState

--- apps/proyecto-integrado/src/app/state/app.state.ts
+++ apps/proyecto-integrado/src/app/state/app.state.ts
@@ -1,7 +1,9 @@
 import { ActionReducerMap } from '@ngrx/store';
 import { AppState } from './interfaces/app.state.interface';
 import { authReducers } from './reducers/auth/auth.reducers';
+import { partiesReducer } from './reducers/parties/parties.reducer';
 
 export const ROOT_REDUCERS: ActionReducerMap<AppState> = {
   auth: authReducers,
+  parties: partiesReducer,
 };
--- apps/proyecto-integrado/src/app/state/interfaces/app.state.interface.ts
+++ apps/proyecto-integrado/src/app/state/interfaces/app.state.interface.ts
@@ -1,5 +1,7 @@
 import { AuthState } from './auth.state.interface';
+import { PartiesState } from './parties.state.interface';
 
 export interface AppState {
   auth: AuthState;
+  parties: PartiesState;
 }
