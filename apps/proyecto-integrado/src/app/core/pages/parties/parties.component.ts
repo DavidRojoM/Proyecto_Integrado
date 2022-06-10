356b61b4f8eafe5f83b0afeeb397267a94cd13da
David Rojo <davidrojo1997@gmail.com>
2022-06-10 22:57:08 +0200

    feat(apps/front): add data retrieving to PartiesComponent

--- apps/proyecto-integrado/src/app/core/pages/parties/parties.component.ts
+++ apps/proyecto-integrado/src/app/core/pages/parties/parties.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../state/interfaces/app.state.interface';
+import { PartiesActions } from '../../../state/actions/parties/parties.actions';
+import { firstValueFrom, map, Observable } from 'rxjs';
+import { PartyOutput } from '../../shared/modules/parties/domain/parties.interface';
 
 @Component({
   selector: 'proyecto-integrado-parties',
@@ -6,7 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parties.component.scss'],
 })
 export class PartiesComponent implements OnInit {
-  constructor() {}
+  allParties$!: Observable<PartyOutput[]>;
+  myParties$!: Observable<PartyOutput[]>;
+  constructor(private readonly store$: Store<AppState>) {
+    this.store$.dispatch(PartiesActions.getPartiesRequest());
+  }
 
-  ngOnInit(): void {}
+  async ngOnInit(): Promise<void> {
+    const userId = await firstValueFrom(
+      this.store$.select((state) => state.auth.user?.id)
+    );
+
+    this.myParties$ = this.store$
+      .select((state) => state.parties.parties)
+      .pipe(
+        map((parties) =>
+          parties.filter((party) =>
+            party.users.some((user) => user.id === userId)
+          )
+        )
+      );
+
+    this.allParties$ = this.store$
+      .select((state) => state.parties.parties)
+      .pipe(
+        map((parties) =>
+          parties.filter((party) => {
+            if (!party.users.length) {
+              return true;
+            }
+            return party.users.some((user) => user.id !== userId);
+          })
+        )
+      );
+    // this.parties$ = this.store$.select((state) => state.parties.parties);
+  }
 }
