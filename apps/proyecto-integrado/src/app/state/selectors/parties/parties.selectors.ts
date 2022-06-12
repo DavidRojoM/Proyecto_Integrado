eafe0a9efe4493410bcba21c031e6f10612637e2
David Rojo <davidrojo1997@gmail.com>
2022-06-12 22:06:53 +0200

    feat(apps/front): add PartiesSelectors#selectPartyById

--- apps/proyecto-integrado/src/app/state/selectors/parties/parties.selectors.ts
+++ apps/proyecto-integrado/src/app/state/selectors/parties/parties.selectors.ts
@@ -1,6 +1,7 @@
 import { AppState } from '../../interfaces/app.state.interface';
 import { createSelector } from '@ngrx/store';
 import { PartiesState } from '../../interfaces/parties.state.interface';
+import { PartyOutput } from '../../../core/shared/modules/parties/domain/parties.interface';
 
 const selectPartiesFeature = (state: AppState) => state.parties;
 
@@ -8,3 +9,10 @@ export const selectParties = createSelector(
   selectPartiesFeature,
   (partiesState: PartiesState) => partiesState.parties
 );
+
+export const selectPartyById = (id: string) =>
+  createSelector(
+    selectPartiesFeature,
+    (partiesState: PartiesState) =>
+      partiesState.parties.find((party) => party.id === id) as PartyOutput
+  );
