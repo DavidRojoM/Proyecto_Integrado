f77631a26395a56a030bb84d6651dfe4c35198e9
David Rojo <davidrojo1997@gmail.com>
2022-06-12 22:08:15 +0200

    feat(apps/front): add ChatGqlRepository

---
+++ apps/proyecto-integrado/src/app/core/shared/modules/comms/services/gql-repository/chat-gql.repository.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { findAllByPartyId } from './queries/find-all-by-party.id';
+import { map, timeout } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { sendMessageMutation } from './mutations/send-message.mutation';
+import { MessageAddedSubscription } from './subscriptions/message-added.subscription';
+import { MessageInput, MessageOutput } from '../../domain/message.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChatGqlRepository {
+  constructor(private readonly apollo: Apollo) {}
+
+  public findAll(partyId: string): Observable<MessageOutput[]> {
+    return this.apollo
+      .query({
+        variables: {
+          partyId,
+        },
+        query: findAllByPartyId,
+      })
+      .pipe(map(({ data }: any) => data.findAllByPartyId));
+  }
+
+  public addOne(message: MessageInput) {
+    return this.apollo
+      .mutate({
+        mutation: sendMessageMutation,
+        variables: {
+          messageInput: { ...message },
+        },
+      })
+      .pipe(map(({ data }: any) => data.sendMessage));
+  }
+
+  public messageAdded(partyId: string) {
+    return this.apollo
+      .subscribe({
+        query: MessageAddedSubscription,
+        variables: {
+          partyId,
+        },
+      })
+      .pipe(map(({ data }: any) => data.messageAdded));
+  }
+}
