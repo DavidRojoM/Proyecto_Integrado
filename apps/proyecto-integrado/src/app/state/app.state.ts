31461dbf96d954b3b9a04453c1cf2c36ba93a89e
David Rojo <davidrojo1997@gmail.com>
2022-06-12 22:07:02 +0200

    feat(apps/front): update AppState

--- apps/proyecto-integrado/src/app/state/app.state.ts
+++ apps/proyecto-integrado/src/app/state/app.state.ts
@@ -2,8 +2,10 @@ import { ActionReducerMap } from '@ngrx/store';
 import { AppState } from './interfaces/app.state.interface';
 import { authReducers } from './reducers/auth/auth.reducers';
 import { partiesReducer } from './reducers/parties/parties.reducer';
+import { messagesReducer } from './reducers/comms/messages.reducers';
 
 export const ROOT_REDUCERS: ActionReducerMap<AppState> = {
   auth: authReducers,
   parties: partiesReducer,
+  messages: messagesReducer,
 };
