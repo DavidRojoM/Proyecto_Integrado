d658c02207ed30f812a5efbc502c0f0bc2188ad9
David Rojo <davidrojo1997@gmail.com>
2022-06-13 23:43:26 +0200

    feat(apps/front): add reducers for joinParty

--- apps/proyecto-integrado/src/app/state/reducers/parties/parties.reducer.ts
+++ apps/proyecto-integrado/src/app/state/reducers/parties/parties.reducer.ts
@@ -22,5 +22,24 @@ export const partiesReducer = createReducer(
     ...state,
     loading: false,
     error: error.error.statusText,
+  })),
+  on(PartiesActions.joinPartyRequest, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(PartiesActions.joinPartySuccess, (state, { user, partyId }) => ({
+    ...state,
+    loading: false,
+    parties: state.parties.map((party) => {
+      if (party.id === partyId) {
+        return { ...party, users: [...party.users, user] };
+      }
+      return party;
+    }),
+  })),
+  on(PartiesActions.joinPartyFailure, (state, { error }: any) => ({
+    ...state,
+    loading: false,
+    error: error.error.statusText,
   }))
 );
