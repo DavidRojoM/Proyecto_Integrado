07d2adf46ed3e8140a78cf88ace5a172e0612aa5
David Rojo <davidrojo1997@gmail.com>
2022-06-14 23:55:31 +0200

    feat(apps/front): remove hanging subscriptions from PartyCard

--- apps/proyecto-integrado/src/app/core/pages/parties/party-card/party-card.component.ts
+++ apps/proyecto-integrado/src/app/core/pages/parties/party-card/party-card.component.ts
@@ -1,51 +1,60 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { PartyOutput } from '../../../shared/modules/parties/domain/parties.interface';
 import { CountryService } from '../../../shared/modules/country/services/country.service';
 import { User } from '../../../shared/modules/users/domain/interfaces/user.interface';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'proyecto-integrado-party-card',
   templateUrl: './party-card.component.html',
   styleUrls: ['./party-card.component.scss'],
 })
-export class PartyCardComponent implements OnInit {
+export class PartyCardComponent implements OnInit, OnDestroy {
   @Output() joinPartyEvent = new EventEmitter<string>();
   @Output() leavePartyEvent = new EventEmitter<string>();
   @Input() userInParty!: boolean;
   @Input() party!: PartyOutput;
   countryFlag!: string;
-  pendingUsers: User[] = [];
-  readyUsers: User[] = [];
+  // pendingUsers: User[] = [];
+  // readyUsers: User[] = [];
 
   organizer: User | undefined;
+
+  subscriptions: Subscription[] = [];
 
   constructor(private readonly countryService: CountryService) {}
 
   ngOnInit(): void {
-    this.countryService
+    const countryFlagSubscription = this.countryService
       .findOne(this.party.trip?.destination?.name as string)
       .subscribe((country) => {
         this.countryFlag = country?.flag as string;
       });
-    this.pendingUsers = this.party.users.filter(
-      (user) => user.status === 'PENDING'
-    );
-    this.readyUsers = this.party.users.filter(
-      (user) => user.status === 'READY'
-    );
+    this.subscriptions = [...this.subscriptions, countryFlagSubscription];
     this.organizer = this.party.users.find(
       (user) => user.status === 'ORGANIZER'
     );
+    console.log('organizer', this.organizer);
   }
 
-  // joinParty(partyId: string): {};
-  //
-  // leaveParty(partyId: string): {};
+  ngOnDestroy() {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+  }
+
   joinParty() {
     this.joinPartyEvent.emit(this.party.id);
   }
 
   leaveParty() {
-    this.joinPartyEvent.emit(this.party.id);
+    this.leavePartyEvent.emit(this.party.id);
   }
 }
