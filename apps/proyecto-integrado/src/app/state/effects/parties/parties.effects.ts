797c47f0090a4d4b85b117e2bc1477d0f71f824c
David Rojo <davidrojo1997@gmail.com>
2022-06-15 23:57:17 +0200

    feat(apps/front): update PartiesEffects

--- apps/proyecto-integrado/src/app/state/effects/parties/parties.effects.ts
+++ apps/proyecto-integrado/src/app/state/effects/parties/parties.effects.ts
@@ -4,13 +4,18 @@ import { PartiesService } from '../../../core/shared/modules/parties/services/pa
 import { SnackbarService } from '../../../core/shared/services/snackbar.service';
 import { catchError, map, of, switchMap, tap, withLatestFrom } from 'rxjs';
 import { PartiesActionTypes } from '../../actions/parties/parties-action.types.enum';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../interfaces/app.state.interface';
+import { selectUser } from '../../selectors/auth/auth.selectors';
+import { PartyInput } from '../../../core/shared/modules/parties/domain/parties.interface';
 
 @Injectable()
 export class PartiesEffects {
   constructor(
     private readonly actions$: Actions,
     private readonly partiesService: PartiesService,
-    private readonly snackbarService: SnackbarService
+    private readonly snackbarService: SnackbarService,
+    private readonly store$: Store<AppState>
   ) {}
 
   findAllRequest$ = createEffect(() =>
@@ -123,5 +128,87 @@ export class PartiesEffects {
     {
       dispatch: false,
     }
+  );
+
+  becomeOrganizerRequest$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(PartiesActionTypes.BECOME_ORGANIZER_REQUEST),
+      switchMap(({ userId, partyId }) =>
+        this.partiesService.becomeOrganizer(userId, partyId).pipe(
+          map(({ user, party }) => ({
+            type: PartiesActionTypes.BECOME_ORGANIZER_SUCCESS,
+            user,
+            party,
+          })),
+          catchError((error) => {
+            return of({
+              type: PartiesActionTypes.BECOME_ORGANIZER_FAILURE,
+              error,
+            });
+          })
+        )
+      )
+    )
+  );
+
+  createPartyRequest$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(PartiesActionTypes.CREATE_PARTY_REQUEST),
+      switchMap(({ name }) =>
+        this.partiesService.createParty(name).pipe(
+          map((party) => ({
+            type: PartiesActionTypes.CREATE_PARTY_SUCCESS,
+            party,
+          })),
+          catchError((error) => {
+            return of({
+              type: PartiesActionTypes.CREATE_PARTY_FAILURE,
+              error,
+            });
+          })
+        )
+      )
+    )
+  );
+
+  createPartyFailure$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(PartiesActionTypes.CREATE_PARTY_FAILURE),
+        tap(() => {
+          this.snackbarService.open('Could create the party', 'DISMISS', 2000);
+        })
+      ),
+    {
+      dispatch: false,
+    }
+  );
+
+  createPartySuccess$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(PartiesActionTypes.CREATE_PARTY_SUCCESS),
+      withLatestFrom(this.store$.select(selectUser)),
+      map(
+        ([{ party }, user]) => ({
+          type: PartiesActionTypes.JOIN_PARTY_REQUEST,
+          userId: user.id,
+          partyId: (party as PartyInput).id,
+        })
+        // switchMap(([{ id }, user]) =>
+        //   this.partiesService.joinParty(id, user.id).pipe(
+        //     map((party) => ({
+        //       type: PartiesActionTypes.JOIN_PARTY_REQUEST,
+        //       party,
+        //     })),
+        //     catchError((error) => {
+        //       return of({
+        //         type: PartiesActionTypes.JOIN_PARTY_FAILURE,
+        //         error,
+        //       });
+        //     })
+        //   )
+        // )
+      )
+    )
   );
 }
