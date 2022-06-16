7127260f1ebd7a707a66e59be53cccdfabb8afce
David Rojo <davidrojo1997@gmail.com>
2022-06-17 01:27:49 +0200

    feat(libs/shared): add findById to TripsRepository

--- libs/shared/src/lib/database/repositories/trips.repository.ts
+++ libs/shared/src/lib/database/repositories/trips.repository.ts
@@ -1,5 +1,10 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { InsertTrip, Trip, TripEntity } from '@proyecto-integrado/shared';
+import {
+  FindTrip,
+  InsertTrip,
+  Trip,
+  TripEntity,
+} from '@proyecto-integrado/shared';
 
 @EntityRepository(TripEntity)
 export class TripsRepository extends Repository<TripEntity> {
@@ -29,6 +34,30 @@ export class TripsRepository extends Repository<TripEntity> {
     return {
       ok: true,
       value: Trip.entityToModel(entity),
+    };
+  }
+
+  async findById(id: string): Promise<FindTrip> {
+    let result;
+    try {
+      result = await this.createQueryBuilder('trip')
+        .leftJoinAndSelect('trip.destination', 'destination')
+        .leftJoinAndSelect('trip.hotel', 'hotel')
+        .leftJoinAndSelect('trip.transport', 'transport')
+        .where('trip.id = :id', { id })
+        .getOneOrFail();
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: e.errno,
+          statusText: e.sqlMessage,
+        },
+      };
+    }
+    return {
+      ok: true,
+      value: Trip.entityToModel(result),
     };
   }
 }
