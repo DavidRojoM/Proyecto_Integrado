7e1e1c756756bcf2ff42395a4a20c323c94b601e
David Rojo <davidrojo1997@gmail.com>
2022-06-17 23:13:13 +0200

    build(app): add hotels.sql

---
+++ tools/docker/hotels.sql
@@ -0,0 +1,7 @@
+INSERT INTO `hotels` (`id`, `name`,`address`,`phone`,`nightPrice`,`image`) VALUES
+(1, 'Hotel 3 estrellas', 'fake address 1', '123456789', 70, 'https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+(2, 'Hotel 5 estrellas', 'fake address 2', '123456789', 190, 'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+(3, 'Hotel 4 estrellas', 'fake address 3', '123456789', 100, 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+(4, 'Hotel 2 estrellas', 'fake address 4', '123456789', 50, 'https://images.unsplash.com/photo-1576354302919-96748cb8299e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=629&q=80'),
+(5, 'Hostal', 'fake address 5', '123456789', 30, 'https://images.unsplash.com/photo-1652204081868-17c77593893e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80');
+
