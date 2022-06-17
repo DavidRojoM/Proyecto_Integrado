cddaf2f17e2774e1405659c041b256ecd9cae562
David Rojo <davidrojo1997@gmail.com>
2022-06-17 23:14:25 +0200

    feat(libs/shared): add BalancesRepository impl

--- libs/shared/src/lib/database/repositories/balances.repository.ts
+++ libs/shared/src/lib/database/repositories/balances.repository.ts
@@ -1,5 +1,41 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { BalanceEntity } from '@proyecto-integrado/shared';
+import {
+  BalanceEntity,
+  ChangeBalancesResponse,
+  User,
+} from '@proyecto-integrado/shared';
 
 @EntityRepository(BalanceEntity)
-export class BalancesRepository extends Repository<BalanceEntity> {}
+export class BalancesRepository extends Repository<BalanceEntity> {
+  async updateBalances(config: {
+    user: User;
+    amount: number;
+  }): Promise<ChangeBalancesResponse> {
+    const userEntity = User.modelToEntity(config.user);
+    try {
+      await this.update(
+        {
+          user: userEntity,
+        },
+        {
+          amount: config.amount,
+        }
+      );
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: e.errno,
+          statusText: e.sqlMessage,
+        },
+      };
+    }
+    return {
+      ok: true,
+      value: {
+        userId: userEntity.id,
+        amount: config.amount,
+      },
+    };
+  }
+}
