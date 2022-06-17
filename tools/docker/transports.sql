e7f692baa0a5c4f5cba6a011ea0d1f2531a0c058
David Rojo <davidrojo1997@gmail.com>
2022-06-17 23:13:18 +0200

    build(app): add transports.sql

---
+++ tools/docker/transports.sql
@@ -0,0 +1,9 @@
+INSERT INTO `transports` (`id`, `name`,`brand`,`price`,`type`)  VALUES
+(1,'Tren de la bruja','La feria',5,'TRAIN'),
+(2,'Autobus escolar','Colegio',10,'BUS'),
+(3,'Autobus de la feria','La feria',10,'BUS'),
+(4,'Jet privado','Iberia',4000,'PLANE'),
+(6,'Avion comercial','Ryanair',120,'PLANE'),
+(7,'Avion primera clase','Iberia',400,'PLANE'),
+(8,'Avanza bus','Avanza',7.50,'BUS'),
+(9,'Metro malaga','MM',5,'TRAIN')
