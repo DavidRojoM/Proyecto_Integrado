3acd0711d6a03c94ab8c100db80a42b85af01f9b
David Rojo <davidrojo1997@gmail.com>
2022-06-18 23:57:47 +0200

    feat(apps/front): update app.state.interface.ts

--- apps/proyecto-integrado/src/app/state/interfaces/app.state.interface.ts
+++ apps/proyecto-integrado/src/app/state/interfaces/app.state.interface.ts
@@ -2,10 +2,12 @@ import { AuthState } from './auth.state.interface';
 import { PartiesState } from './parties.state.interface';
 import { MessagesState } from './messages.state.interface';
 import { TripsState } from './trips.state.interface';
+import { WishlistsState } from './wishlists.state.interface';
 
 export interface AppState {
   auth: AuthState;
   parties: PartiesState;
   messages: MessagesState;
   trips: TripsState;
+  wishlists: WishlistsState;
 }
---
+++ apps/proyecto-integrado/src/app/state/interfaces/wishlists.state.interface.ts
@@ -0,0 +1,5 @@
+import { Wishlist } from '../../core/shared/modules/wishlists/domain/interfaces/wishlist.interface';
+
+export interface WishlistsState {
+  wishlists: Wishlist[];
+}
