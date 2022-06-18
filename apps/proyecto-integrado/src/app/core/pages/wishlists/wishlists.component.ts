1f109474f08a6c4a594cb4011ef7cf82c92e9897
David Rojo <davidrojo1997@gmail.com>
2022-06-18 23:59:55 +0200

    feat(apps/front): update WishlistComponent impl

--- apps/proyecto-integrado/src/app/core/pages/wishlists/wishlists.component.ts
+++ apps/proyecto-integrado/src/app/core/pages/wishlists/wishlists.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../state/interfaces/app.state.interface';
+import { WishlistsActions } from '../../../state/actions/wishlists/wishlists.actions';
+import { selectWishlists } from '../../../state/selectors/wishlists/wishlists.selectors';
+import { AddWishlistDialogComponent } from './add-wishlist-dialog/add-wishlist-dialog.component';
+import { selectUser } from '../../../state/selectors/auth/auth.selectors';
+import { map } from 'rxjs';
+import { PartiesActions } from '../../../state/actions/parties/parties.actions';
 
 @Component({
   selector: 'proyecto-integrado-wishlists',
@@ -6,7 +15,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wishlists.component.scss'],
 })
 export class WishlistsComponent implements OnInit {
-  constructor() {}
+  wishlists$ = this.store$.select(selectWishlists);
+  parties$ = this.store$
+    .select(selectUser)
+    .pipe(map((user) => user.parties || []));
 
-  ngOnInit(): void {}
+  constructor(
+    private readonly dialog: MatDialog,
+    private readonly store$: Store<AppState>
+  ) {}
+
+  ngOnInit(): void {
+    this.store$.dispatch(WishlistsActions.getAllWishlistsRequest());
+  }
+
+  createWishlist(): void {
+    this.dialog.open(AddWishlistDialogComponent);
+  }
+
+  addToParty(config: { userId: string; partyId: string }) {
+    //TODO:CAMBIAR POR OTRA ACCION PARA NO GENERAR CONFLICTOS CON EL REDUCER
+    this.store$.dispatch(PartiesActions.addToPartyRequest(config));
+  }
 }
--- apps/proyecto-integrado/src/app/core/pages/wishlists/wishlists.module.ts
+++ apps/proyecto-integrado/src/app/core/pages/wishlists/wishlists.module.ts
@@ -3,9 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { WishlistsRoutingModule } from './wishlists-routing.module';
 import { WishlistsComponent } from './wishlists.component';
+import { AddWishlistDialogComponent } from './add-wishlist-dialog/add-wishlist-dialog.component';
+import { MaterialModule } from '../../shared/modules/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
+import { GenderPipe } from '../../shared/pipes/gender.pipe';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
 @NgModule({
-  declarations: [WishlistsComponent],
-  imports: [CommonModule, WishlistsRoutingModule],
+  declarations: [
+    WishlistsComponent,
+    AddWishlistDialogComponent,
+    GenderPipe,
+    WishlistComponent,
+  ],
+  imports: [
+    CommonModule,
+    WishlistsRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
 })
 export class WishlistsModule {}
