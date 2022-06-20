1f72e717afcc391301bcadc5fdee6ae2a611348a
David Rojo <davidrojo1997@gmail.com>
2022-06-20 02:50:25 +0200

    feat(apps/front): add BackOffice actions to TripsReducers

--- apps/proyecto-integrado/src/app/state/reducers/trips/trips.reducers.ts
+++ apps/proyecto-integrado/src/app/state/reducers/trips/trips.reducers.ts
@@ -1,6 +1,7 @@
 import { TripsState } from '../../interfaces/trips.state.interface';
 import { createReducer, on } from '@ngrx/store';
 import { TripsActions } from '../../actions/trips/trips.actions';
+import { BackofficeActions } from '../../actions/backoffice/backoffice.actions';
 
 const initialState: TripsState = {
   loading: false,
@@ -78,6 +79,139 @@ export const tripsReducer = createReducer(
     trips: [...state.trips, trip],
   })),
   on(TripsActions.createTripFailure, (state, { error }: any) => ({
+    ...state,
+    loading: false,
+    error: error.error.statusText,
+  })),
+  on(BackofficeActions.addDestinationRequest, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(BackofficeActions.addDestinationSuccess, (state, { destination }) => ({
+    ...state,
+    loading: false,
+    destinations: [...state.destinations, destination],
+  })),
+  on(BackofficeActions.addDestinationFailure, (state, { error }: any) => ({
+    ...state,
+    loading: false,
+    error: error.error.statusText,
+  })),
+  on(BackofficeActions.addTransportRequest, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(BackofficeActions.addTransportSuccess, (state, { transport }) => ({
+    ...state,
+    loading: false,
+    transports: [...state.transports, transport],
+  })),
+  on(BackofficeActions.addTransportFailure, (state, { error }: any) => ({
+    ...state,
+    loading: false,
+    error: error.error.statusText,
+  })),
+  on(BackofficeActions.addHotelRequest, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(BackofficeActions.addHotelSuccess, (state, { hotel }) => ({
+    ...state,
+    loading: false,
+    hotels: [...state.hotels, hotel],
+  })),
+  on(BackofficeActions.addHotelFailure, (state, { error }: any) => ({
+    ...state,
+    loading: false,
+    error: error.error.statusText,
+  })),
+  on(BackofficeActions.deleteDestinationRequest, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(
+    BackofficeActions.deleteDestinationSuccess,
+    (state, { destinationId }) => ({
+      ...state,
+      loading: false,
+      destinations: state.destinations.filter((d) => d.id !== destinationId),
+    })
+  ),
+  on(BackofficeActions.deleteDestinationFailure, (state, { error }: any) => ({
+    ...state,
+    loading: false,
+    error: error.error.statusText,
+  })),
+  on(BackofficeActions.deleteTransportRequest, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(BackofficeActions.deleteTransportSuccess, (state, { transportId }) => ({
+    ...state,
+    loading: false,
+    transports: state.transports.filter((t) => t.id !== transportId),
+  })),
+  on(BackofficeActions.deleteTransportFailure, (state, { error }: any) => ({
+    ...state,
+    loading: false,
+    error: error.error.statusText,
+  })),
+  on(BackofficeActions.deleteHotelRequest, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(BackofficeActions.deleteHotelSuccess, (state, { hotelId }) => ({
+    ...state,
+    loading: false,
+    hotels: state.hotels.filter((h) => h.id !== hotelId),
+  })),
+  on(BackofficeActions.deleteHotelFailure, (state, { error }: any) => ({
+    ...state,
+    loading: false,
+    error: error.error.statusText,
+  })),
+  on(BackofficeActions.editDestinationRequest, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(BackofficeActions.editDestinationSuccess, (state, { destination }) => ({
+    ...state,
+    loading: false,
+    destinations: state.destinations.map((d) =>
+      d.id === destination.id ? destination : d
+    ),
+  })),
+  on(BackofficeActions.editDestinationFailure, (state, { error }: any) => ({
+    ...state,
+    loading: false,
+    error: error.error.statusText,
+  })),
+  on(BackofficeActions.editTransportRequest, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(BackofficeActions.editTransportSuccess, (state, { transport }) => ({
+    ...state,
+    loading: false,
+    transports: state.transports.map((t) =>
+      t.id === transport.id ? transport : t
+    ),
+  })),
+  on(BackofficeActions.editTransportFailure, (state, { error }: any) => ({
+    ...state,
+    loading: false,
+    error: error.error.statusText,
+  })),
+  on(BackofficeActions.editHotelRequest, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(BackofficeActions.editHotelSuccess, (state, { hotel }) => ({
+    ...state,
+    loading: false,
+    hotels: state.hotels.map((h) => (h.id === hotel.id ? hotel : h)),
+  })),
+  on(BackofficeActions.editHotelFailure, (state, { error }: any) => ({
     ...state,
     loading: false,
     error: error.error.statusText,
