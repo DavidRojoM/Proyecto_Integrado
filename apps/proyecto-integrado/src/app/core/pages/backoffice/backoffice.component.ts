c26abbd5b2f7fabd246b0b7ada7539b11a099c09
David Rojo <davidrojo1997@gmail.com>
2022-06-20 03:02:53 +0200

    feat(apps/api): add backoffice page

--- apps/proyecto-integrado/src/app/core/pages/backoffice/backoffice.component.ts
+++ apps/proyecto-integrado/src/app/core/pages/backoffice/backoffice.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../state/interfaces/app.state.interface';
+import {
+  selectAllDestinations,
+  selectAllHotels,
+  selectAllTransports,
+} from '../../../state/selectors/trips/trips.selectors';
+import { TripsActions } from '../../../state/actions/trips/trips.actions';
+import { selectParties } from '../../../state/selectors/parties/parties.selectors';
+import { PartiesActions } from '../../../state/actions/parties/parties.actions';
+import { BackofficeActions } from '../../../state/actions/backoffice/backoffice.actions';
+import { selectUsers } from '../../../state/selectors/backoffice/backoffice-users.selectors';
 
 @Component({
   selector: 'proyecto-integrado-backoffice',
@@ -6,7 +18,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./backoffice.component.scss'],
 })
 export class BackofficeComponent implements OnInit {
-  constructor() {}
+  users$ = this.store$.select(selectUsers);
+  parties$ = this.store$.select(selectParties);
+  hotels$ = this.store$.select(selectAllHotels);
+  destinations$ = this.store$.select(selectAllDestinations);
+  transports$ = this.store$.select(selectAllTransports);
 
-  ngOnInit(): void {}
+  constructor(private readonly store$: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.store$.dispatch(TripsActions.getAllHotelsRequest());
+    this.store$.dispatch(TripsActions.getAllDestinationsRequest());
+    this.store$.dispatch(TripsActions.getAllTransportsRequest());
+    this.store$.dispatch(PartiesActions.getPartiesRequest());
+    this.store$.dispatch(BackofficeActions.findUsersRequest());
+  }
 }
--- apps/proyecto-integrado/src/app/core/pages/backoffice/backoffice.module.ts
+++ apps/proyecto-integrado/src/app/core/pages/backoffice/backoffice.module.ts
@@ -3,9 +3,62 @@ import { CommonModule } from '@angular/common';
 
 import { BackofficeRoutingModule } from './backoffice-routing.module';
 import { BackofficeComponent } from './backoffice.component';
+import { MaterialModule } from '../../shared/modules/material/material.module';
+import { TransportsBackofficeComponent } from './transports/home/transports-backoffice.component';
+import { PartiesBackofficeComponent } from './parties/home/parties-backoffice.component';
+import { HotelsBackofficeComponent } from './hotels/home/hotels-backoffice.component';
+import { DestinationsBackofficeComponent } from './destinations/home/destinations-backoffice.component';
+import { UsersBackofficeComponent } from './users/home/users-backoffice.component';
+import { UsersBackofficeTableComponent } from './users/home/users-backoffice-table/users-backoffice-table.component';
+import { HotelsBackofficeTableComponent } from './hotels/home/hotels-backoffice-table/hotels-backoffice-table.component';
+import { DestinationsBackofficeTableComponent } from './destinations/home/destinations-backoffice-table/destinations-backoffice-table.component';
+import { PartiesBackofficeTableComponent } from './parties/home/parties-backoffice-table/parties-backoffice-table.component';
+import { TransportsBackofficeTableComponent } from './transports/home/transports-backoffice-table/transports-backoffice-table.component';
+import { DestinationsDialogComponent } from './destinations/dialog/destinations-dialog.component';
+import { DestinationsFormComponent } from './destinations/dialog/form/destinations-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HotelsDialogComponent } from './hotels/dialog/hotels-dialog.component';
+import { HotelsFormComponent } from './hotels/dialog/form/hotels-form.component';
+import { TransportsDialogComponent } from './transports/dialog/transports-dialog.component';
+import { TransportsFormComponent } from './transports/dialog/form/transports-form.component';
+import { PartiesDialogComponent } from './parties/dialog/parties-dialog.component';
+import { PartiesFormComponent } from './parties/dialog/form/parties-form.component';
+import { UsersFormComponent } from './users/dialog/form/users-form.component';
+import { NgxFileDragDropModule } from 'ngx-file-drag-drop';
+import { SharedModule } from '../../shared/shared.module';
+import { UsersDialogComponent } from './users/dialog/users-dialog.component';
 
 @NgModule({
-  declarations: [BackofficeComponent],
-  imports: [CommonModule, BackofficeRoutingModule],
+  declarations: [
+    BackofficeComponent,
+    TransportsBackofficeComponent,
+    PartiesBackofficeComponent,
+    HotelsBackofficeComponent,
+    DestinationsBackofficeComponent,
+    UsersBackofficeComponent,
+    UsersBackofficeTableComponent,
+    HotelsBackofficeTableComponent,
+    DestinationsBackofficeTableComponent,
+    PartiesBackofficeTableComponent,
+    TransportsBackofficeTableComponent,
+    DestinationsDialogComponent,
+    DestinationsFormComponent,
+    HotelsDialogComponent,
+    HotelsFormComponent,
+    TransportsDialogComponent,
+    TransportsFormComponent,
+    PartiesDialogComponent,
+    PartiesFormComponent,
+    UsersFormComponent,
+    UsersDialogComponent,
+  ],
+  imports: [
+    CommonModule,
+    BackofficeRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    NgxFileDragDropModule,
+    SharedModule,
+  ],
 })
 export class BackofficeModule {}
