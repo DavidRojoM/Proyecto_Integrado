5eea03ad8e66bb795ce05bf13fc7491ea2afa672
David Rojo <davidrojo1997@gmail.com>
2022-06-20 07:14:44 +0200

    feat(libs/shared): update TripsPayloads

--- libs/shared/src/lib/payloads/trips/trips.payloads.ts
+++ libs/shared/src/lib/payloads/trips/trips.payloads.ts
@@ -22,16 +22,22 @@ export type FindAllDestinationsResponse = Result<
   DestinationDto[],
   ErrorPayload
 >;
+
 export type DeleteTripAggregateResponse = Result<{ id: number }, ErrorPayload>;
 export type InsertTripResponse = Result<Trip, ErrorPayload>;
 export type FindTripResponse = Result<Trip, ErrorPayload>;
 export type InsertTransportResponse = Result<TransportDto, ErrorPayload>;
 export type InsertHotelResponse = Result<HotelDto, ErrorPayload>;
 export type InsertDestinationResponse = Result<DestinationDto, ErrorPayload>;
+export type UpdateTransportResponse = Result<TransportDto, ErrorPayload>;
+export type UpdateHotelResponse = Result<HotelDto, ErrorPayload>;
+export type UpdateDestinationResponse = Result<DestinationDto, ErrorPayload>;
 
+export type UpdateTransport = Result<Transport, ErrorPayload>;
+export type UpdateHotel = Result<Hotel, ErrorPayload>;
+export type UpdateDestination = Result<Destination, ErrorPayload>;
 export type InsertTrip = Result<Trip, ErrorPayload>;
 export type FindTrip = Result<Trip, ErrorPayload>;
-
 export type InsertTransport = Result<Transport, ErrorPayload>;
 export type InsertHotel = Result<Hotel, ErrorPayload>;
 export type InsertDestination = Result<Destination, ErrorPayload>;
