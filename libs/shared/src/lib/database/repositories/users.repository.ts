3cd969dfb551ceb84100b3fd82ad919200854f61
David Rojo <davidrojo1997@gmail.com>
2022-06-20 05:48:11 +0200

    feat(libs.shared): add updateOne impl at UsersRepository

--- libs/shared/src/lib/database/repositories/users.repository.ts
+++ libs/shared/src/lib/database/repositories/users.repository.ts
@@ -8,6 +8,7 @@ import {
   UserEntity,
 } from '@proyecto-integrado/shared';
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(UserEntity)
 export class UsersRepository extends Repository<UserEntity> {
@@ -56,6 +57,52 @@ export class UsersRepository extends Repository<UserEntity> {
     return {
       ok: true,
       value: User.entityToModel(entity),
+    };
+  }
+
+  async updateOne(userToUpdate: User): Promise<InsertUser> {
+    const entity = User.modelToEntity(userToUpdate);
+    let result;
+    try {
+      const user = await this.findByUsernameOrEmail(
+        entity.username,
+        entity.email
+      );
+      if (user && user?.id !== entity.id) {
+        return {
+          ok: false,
+          error: {
+            statusCode: 400,
+            statusText: 'User already exists',
+          },
+        };
+      }
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(entity?.password || '', salt);
+
+      await this.update(entity.id, {
+        ...(entity.username && { username: entity.username }),
+        ...(entity.email && { email: entity.email }),
+        ...(entity.image && { image: entity.image }),
+        ...(entity.email && { email: entity.email }),
+        ...(entity.password && { password: user.password || hashedPassword }),
+        ...(entity.role && { role: entity.role }),
+        ...(entity.nationality && { nationality: entity.nationality }),
+        ...(entity.bankAccount && { bankAccount: entity.bankAccount }),
+      });
+      result = await this.findOneOrFail(entity.id);
+    } catch (e) {
+      return {
+        ok: false,
+        error: {
+          statusCode: e.errno,
+          statusText: e.sqlMessage,
+        },
+      };
+    }
+    return {
+      ok: true,
+      value: User.entityToModel(result),
     };
   }
 
@@ -142,5 +189,15 @@ export class UsersRepository extends Repository<UserEntity> {
         userId: id,
       },
     };
+  }
+  private removeEmptyProperties(obj: any): any {
+    const object = {};
+    Object.keys(obj).forEach((key) => {
+      if (!obj[key]) {
+        return;
+      }
+      object[key] = obj[key];
+    });
+    return object;
   }
 }
