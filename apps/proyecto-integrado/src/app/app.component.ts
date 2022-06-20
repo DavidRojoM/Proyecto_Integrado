9a48ceec09379ab62ef0560e04fa210fc92753a1
David Rojo <davidrojo1997@gmail.com>
2022-06-20 02:52:05 +0200

    feat(apps/front): display avatar at sidebar

--- apps/proyecto-integrado/src/app/app.component.ts
+++ apps/proyecto-integrado/src/app/app.component.ts
@@ -4,10 +4,10 @@ import { AuthActions } from './state/actions/auth/auth.actions';
 import { Store } from '@ngrx/store';
 import { AppState } from './state/interfaces/app.state.interface';
 import { MatSidenav } from '@angular/material/sidenav';
-import { Observable } from 'rxjs';
 import {
   selectIsAdmin,
   selectIsAuthenticated,
+  selectUser,
 } from './state/selectors/auth/auth.selectors';
 import { selectBalances } from './state/selectors/balances/balances.selectors';
 import { MatDialog } from '@angular/material/dialog';
@@ -19,9 +19,10 @@ import { AddBalancesDialogComponent } from './core/dialogs/add-balances-dialog/a
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnDestroy {
-  isAuhenticated!: Observable<boolean>;
-  isAdmin!: Observable<boolean>;
-  userBalances!: Observable<number>;
+  isAuthenticated = this.store$.select(selectIsAuthenticated);
+  me = this.store$.select(selectUser);
+  isAdmin = this.store$.select(selectIsAdmin);
+  userBalances = this.store$.select(selectBalances);
 
   mobileQuery: MediaQueryList;
 
@@ -36,9 +37,6 @@ export class AppComponent implements OnDestroy {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
-    this.isAuhenticated = this.store$.select(selectIsAuthenticated);
-    this.isAdmin = this.store$.select(selectIsAdmin);
-    this.userBalances = this.store$.select(selectBalances);
   }
 
   ngOnDestroy(): void {
