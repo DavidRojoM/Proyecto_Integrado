6205bcc8ab6095e0c1452cd1654fdfc535b51a2f
David Rojo <davidrojo1997@gmail.com>
2022-06-20 02:46:53 +0200

    feat(apps/front): add crud methods at UsersService

--- apps/proyecto-integrado/src/app/core/shared/modules/users/services/users.service.ts
+++ apps/proyecto-integrado/src/app/core/shared/modules/users/services/users.service.ts
@@ -1,7 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../domain/interfaces/user.interface';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  constructor() {}
+  private readonly url = `${environment.GATEWAY_URL}/users`;
+
+  constructor(private readonly http: HttpClient) {}
+
+  findUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.url);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(this.url, user);
+  }
+
+  deleteUser(userId: string): Observable<{ userId: string }> {
+    return this.http.delete<{ userId: string }>(`${this.url}/${userId}`);
+  }
 }
